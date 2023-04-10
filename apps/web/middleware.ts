784a187185ddc6e1a450beaff87d43bb4fcae82c
João Neto <joao@neto.pt>
2023-04-10 20:01:43 +0200

    refactor auth into package

---
+++ apps/web/middleware.ts
@@ -0,0 +1,21 @@
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(
+  function middleware(req) {
+    if (!req.nextauth.token) {
+      const url = req.nextUrl.clone();
+      url.pathname = "/";
+      return NextResponse.rewrite(url);
+    }
+  },
+  {
+    callbacks: {
+      authorized: ({ token, req }) => {
+        return true;
+      },
+    },
+  }
+);
+
+export const config = { matcher: ["/mailbox"] };
