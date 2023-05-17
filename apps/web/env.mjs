738e5cd6a585d340e3dd58327ffc1c0fcc11c9d7
João Neto <joao@neto.pt>
2023-05-17 16:32:42 +0200

    chore cleanup

--- apps/web/env.mjs
+++ apps/web/env.mjs
@@ -52,7 +52,7 @@ const merged = server.merge(client);
 
 let env = /** @type {MergedOutput} */ (process.env);
 
-if (!!process.env.SKIP_ENV_VALIDATION == false) {
+if (!!process.env.SKIP_ENV_VALIDATION === false) {
   const isServer = typeof window === "undefined";
 
   const parsed = /** @type {MergedSafeParseReturn} */ (
@@ -74,7 +74,7 @@ if (!!process.env.SKIP_ENV_VALIDATION == false) {
       if (typeof prop !== "string") return undefined;
       // Throw a descriptive error if a server-side env var is accessed on the client
       // Otherwise it would just be returning `undefined` and be annoying to debug
-      if (!isServer && !prop.startsWith("NEXT_PUBLIC_"))
+      if (!isServer && !prop.startsWith("NEXT_PUBLIC_") && !["NODE_ENV"].includes(prop))
         throw new Error(
           process.env.NODE_ENV === "production"
             ? "❌ Attempted to access a server-side environment variable on the client"
