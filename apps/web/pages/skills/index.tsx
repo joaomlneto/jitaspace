941bbecfc8bff81c41279f496b0ebb375985687d
João Neto <joao@neto.pt>
2023-06-13 14:56:09 +0200

    fix build: cleanup unused vars

--- apps/web/pages/skills/index.tsx
+++ apps/web/pages/skills/index.tsx
@@ -1,8 +1,6 @@
 import React, { type ReactElement } from "react";
 import { Container, Group, Stack, Title } from "@mantine/core";
-import { useSession } from "next-auth/react";
 
-import { useGetCharactersCharacterIdSkillqueue } from "@jitaspace/esi-client";
 import { SkillsIcon } from "@jitaspace/eve-icons";
 
 import {
@@ -12,17 +10,6 @@ import {
 import { MainLayout } from "~/layouts";
 
 export default function Page() {
-  const { data: session } = useSession();
-  const { data: attributes } = useGetCharactersCharacterIdSkillqueue(
-    session?.user?.id ?? 1,
-    {},
-    {
-      swr: {
-        enabled: !!session?.user?.id,
-      },
-    },
-  );
-
   return (
     <Container size="xl">
       <Stack spacing="xl">
