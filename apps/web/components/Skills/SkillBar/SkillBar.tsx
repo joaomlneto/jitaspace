6af3c3d48219878babb76a9f935492b8d21a5463
João Neto <joao@neto.pt>
2023-06-14 17:54:57 +0200

    memoize SkillBar

--- apps/web/components/Skills/SkillBar/SkillBar.tsx
+++ apps/web/components/Skills/SkillBar/SkillBar.tsx
@@ -1,3 +1,4 @@
+import { memo } from "react";
 import { createStyles, Group, Stack, Text, Tooltip } from "@mantine/core";
 
 const useStyles = createStyles((theme) => ({
@@ -28,52 +29,56 @@ export type SkillBarProps = {
   requirementType?: "queued" | "missing" | "missingStrong";
 };
 
-export function SkillBar({
-  activeLevel = 0,
-  requiredLevel = 5,
-  requirementType = "missing",
-}: SkillBarProps) {
-  const { classes } = useStyles();
+export const SkillBar = memo(
+  ({
+    activeLevel = 0,
+    requiredLevel = 5,
+    requirementType = "missing",
+  }: SkillBarProps) => {
+    const { classes } = useStyles();
 
-  const isTrained = (level: number) => level <= activeLevel;
-  const isRequired = (level: number) => requiredLevel && level <= requiredLevel;
+    const isTrained = (level: number) => level <= activeLevel;
+    const isRequired = (level: number) =>
+      requiredLevel && level <= requiredLevel;
 
-  const getIcon = (level: number) => (
-    <div
-      key={level}
-      style={{
-        width: 8,
-        height: 8,
-        margin: "1px",
-      }}
-      className={((level) => {
-        if (isTrained(level)) return classes.trained;
-        if (isRequired(level)) return classes[requirementType];
-        return classes.notRequired;
-      })(level)}
-    />
-  );
+    const getIcon = (level: number) => (
+      <div
+        key={level}
+        style={{
+          width: 8,
+          height: 8,
+          margin: "1px",
+        }}
+        className={((level) => {
+          if (isTrained(level)) return classes.trained;
+          if (isRequired(level)) return classes[requirementType];
+          return classes.notRequired;
+        })(level)}
+      />
+    );
 
-  return (
-    <Tooltip
-      label={
-        <Stack spacing={0}>
-          <Group position="apart">
-            <Text size="xs">Level trained:</Text>
-            <Text size="xs">{activeLevel}</Text>
-          </Group>
-          {requiredLevel && (
+    return (
+      <Tooltip
+        label={
+          <Stack spacing={0}>
             <Group position="apart">
-              <Text size="xs">Level required:</Text>
-              <Text size="xs">{requiredLevel}</Text>
+              <Text size="xs">Level trained:</Text>
+              <Text size="xs">{activeLevel}</Text>
             </Group>
-          )}
-        </Stack>
-      }
-    >
-      <Group spacing={0}>
-        {[1, 2, 3, 4, 5].map((level) => getIcon(level))}
-      </Group>
-    </Tooltip>
-  );
-}
+            {requiredLevel && (
+              <Group position="apart">
+                <Text size="xs">Level required:</Text>
+                <Text size="xs">{requiredLevel}</Text>
+              </Group>
+            )}
+          </Stack>
+        }
+      >
+        <Group spacing={0}>
+          {[1, 2, 3, 4, 5].map((level) => getIcon(level))}
+        </Group>
+      </Tooltip>
+    );
+  },
+);
+SkillBar.displayName = "SkillBar";
