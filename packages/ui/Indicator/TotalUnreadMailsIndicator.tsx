dc8c6d38bc52e5a0488e4ef4474255823bcbff56
João Neto <joao@neto.pt>
2023-06-18 22:59:07 +0200

    Fix TotalUnreadMailsIndicator 4xx requests

--- packages/ui/Indicator/TotalUnreadMailsIndicator.tsx
+++ packages/ui/Indicator/TotalUnreadMailsIndicator.tsx
@@ -7,21 +7,23 @@ import {
 } from "@jitaspace/esi-client";
 
 export const TotalUnreadMailsIndicator = memo((props: IndicatorProps) => {
-  const { characterId, isTokenValid } = useEsiClientContext();
+  const { characterId, isTokenValid, scopes } = useEsiClientContext();
+
+  const canMakeQuery = isTokenValid && scopes.includes("esi-mail.read_mail.v1");
 
   const { data: labels } = useGetCharactersCharacterIdMailLabels(
     characterId ?? 1,
     undefined,
     {
       swr: {
-        enabled: isTokenValid,
+        enabled: canMakeQuery,
       },
     },
   );
 
   return (
     <Indicator
-      disabled={!isTokenValid || labels?.data.total_unread_count === 0}
+      disabled={!canMakeQuery || labels?.data.total_unread_count === 0}
       label={`${labels?.data.total_unread_count ?? ""}`}
       {...props}
     />
