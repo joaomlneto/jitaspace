3eed3cde64d5036cff1569a0eb65832e05c6e907
João Neto <joao@neto.pt>
2023-06-28 00:04:49 +0200

    market-groups api: run on edge environment

--- apps/web/pages/api/esi/market-groups.ts
+++ apps/web/pages/api/esi/market-groups.ts
@@ -2,13 +2,13 @@ import { type NextApiRequest, type NextApiResponse } from "next";
 import axios, { HttpStatusCode } from "axios";
 import pLimit from "p-limit";
 
-import {
-  getMarketsGroups,
-  getMarketsGroupsMarketGroupId,
-  type GetMarketsGroupsMarketGroupId200,
-} from "@jitaspace/esi-client";
+import { type GetMarketsGroupsMarketGroupId200 } from "@jitaspace/esi-client";
 
-import { ESI_BASE_URL, TRANQUILITY_DOWNTIME_SECONDS } from "~/config/constants";
+import { ESI_BASE_URL } from "~/config/constants";
+
+export const config = {
+  runtime: "edge",
+};
 
 export default async function NextApiRouteHandler(
   req: NextApiRequest,
@@ -22,20 +22,18 @@ export default async function NextApiRouteHandler(
   axios.defaults.baseURL = ESI_BASE_URL;
 
   // get list of market group IDs
-  const marketGroupsResponse = await getMarketsGroups();
-
-  if (marketGroupsResponse.status !== HttpStatusCode.Ok) {
-    return res
-      .status(HttpStatusCode.InternalServerError)
-      .json({ error: "Could not fetch market groups" });
-  }
+  const marketGroupsResponse = await fetch(
+    "https://esi.evetech.net/latest/markets/groups",
+  ).then((res) => res.json() as Promise<number[]>);
 
   // get the details of all market groups
   const limit = pLimit(100);
-  let remaining = marketGroupsResponse.data.length;
-  const promises = marketGroupsResponse.data.map((marketGroupId) =>
+  let remaining = marketGroupsResponse.length;
+  const promises = marketGroupsResponse.map((marketGroupId) =>
     limit(async () => {
-      const result = await getMarketsGroupsMarketGroupId(marketGroupId);
+      const result = await fetch(
+        `https://esi.evetech.net/latest/markets/groups/${marketGroupId}`,
+      ).then((res) => res.json() as Promise<GetMarketsGroupsMarketGroupId200>);
       remaining--;
       console.log("remaining", remaining);
       return result;
@@ -52,29 +50,29 @@ export default async function NextApiRouteHandler(
   // restructure data into a map of market group ids to market group details
   const marketGroupsMap: Record<number, GetMarketsGroupsMarketGroupId200> = {};
   results.forEach((result) => {
-    marketGroupsMap[result.data.market_group_id] = result.data;
+    marketGroupsMap[result.market_group_id] = result;
   });
 
+  /*
   // compute expiration date for cache
   // @ts-expect-error headers should be there!
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
   const expirationDate = new Date(marketGroupsResponse.headers.get("Expires"));
   const now = new Date();
 
   const secondsUntilExpirationDate = Math.floor(
     (expirationDate.getTime() - now.getTime()) / 1000,
-  );
+  );*/
 
   const response = {
     marketGroups: marketGroupsMap,
   };
 
-  return res
+  return Response.json(response);
+  /*
     .appendHeader(
       "Cache-Control",
       `public, s-maxage=${
         secondsUntilExpirationDate + TRANQUILITY_DOWNTIME_SECONDS
       }, stale-while-revalidate=3600`,
-    )
-    .json(response);
+    )*/
 }
