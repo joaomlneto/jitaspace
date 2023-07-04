286a07d26a08d04ddb6db73ba28c0cd8fb8f7ec2
João Neto <joao@neto.pt>
2023-07-04 18:03:43 +0100

    Update dev.tsx

--- apps/web/pages/dev.tsx
+++ apps/web/pages/dev.tsx
@@ -1,24 +1,14 @@
 import React, { type ReactElement } from "react";
-import { Container, JsonInput, Stack } from "@mantine/core";
-import { useSession } from "next-auth/react";
+import { Container, Stack } from "@mantine/core";
 
 import { EsiClientStateCard } from "~/components/EsiClient";
 import { MainLayout } from "~/layouts";
 
 export default function Page() {
-  const { data: session } = useSession();
-
   return (
     <Container size="lg">
       <Stack>
         <EsiClientStateCard />
-        <JsonInput
-          label="Session Data"
-          value={JSON.stringify(session, null, 2)}
-          autosize
-          readOnly
-          maxRows={50}
-        />
       </Stack>
     </Container>
   );
