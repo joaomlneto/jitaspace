4b0c56caaee0f248f8269e6ef99f1d47a62f537f
João Neto <joao@neto.pt>
2023-07-11 15:00:23 +0100

    add planet page

---
+++ apps/web/pages/planet/[planetId].tsx
@@ -0,0 +1,55 @@
+import React, { type ReactElement } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { Anchor, Container, Group, Stack, Text, Title } from "@mantine/core";
+
+import { useGetUniversePlanetsPlanetId } from "@jitaspace/esi-client";
+import {
+  SolarSystemName,
+  SolarSystemSecurityStatusBadge,
+  TypeAvatar,
+  TypeName,
+} from "@jitaspace/ui";
+
+import { MailLayout } from "~/layouts";
+
+export default function Page() {
+  const router = useRouter();
+  const planetId = router.query.planetId as string;
+  const { data: planet } = useGetUniversePlanetsPlanetId(parseInt(planetId));
+
+  return (
+    <Container size="sm">
+      <Stack>
+        <Group spacing="xl">
+          <TypeAvatar typeId={planet?.data.type_id} size="xl" radius={256} />
+          <Title order={3}>
+            <Text>{planet?.data.name}</Text>
+          </Title>
+        </Group>
+        <Group position="apart">
+          <Text>Solar System</Text>
+          <Group spacing="xs">
+            <SolarSystemSecurityStatusBadge
+              solarSystemId={planet?.data.system_id}
+              size="sm"
+            />
+            <Anchor component={Link} href={`/system/${planet?.data.system_id}`}>
+              <SolarSystemName span solarSystemId={planet?.data.system_id} />
+            </Anchor>
+          </Group>
+        </Group>
+        <Group position="apart">
+          <Text>Planet Type</Text>
+          <Anchor component={Link} href={`/type/${planet?.data.type_id}`}>
+            <TypeName span typeId={planet?.data.type_id} />
+          </Anchor>
+        </Group>
+      </Stack>
+    </Container>
+  );
+}
+
+Page.getLayout = function getLayout(page: ReactElement) {
+  return <MailLayout>{page}</MailLayout>;
+};
