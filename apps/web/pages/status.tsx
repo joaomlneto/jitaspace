74bd607c2a6034522d8ce5c743779d2ef12d3cfe
João Neto <joao@neto.pt>
2023-07-16 19:12:37 +0100

    simplify status page

--- apps/web/pages/status.tsx
+++ apps/web/pages/status.tsx
@@ -98,72 +98,80 @@ export default function Page() {
   return (
     <Container size="sm">
       <Stack>
-        <Title>Status</Title>
-        <Title order={3}>Jita</Title>
-        <Group position="apart">
-          <Text>Vercel</Text>
-          <Group>
-            {vercelStatusIsLoading && "Checking..."}
-            {vercelStatusData?.status.description}
+        <Stack spacing="xs">
+          <Title>Status</Title>
+          <Title order={3}>Jita</Title>
+          <Group position="apart">
+            <Text>Vercel Platform</Text>
+            <Group>
+              {vercelStatusIsLoading && "Checking..."}
+              {vercelStatusData?.status.description}
+            </Group>
           </Group>
-        </Group>
-        <Group position="apart">
-          <Text>SDE API Updated On</Text>
-          {/* FIXME: shouldnt be hardcoded! :) */}
-          <Text>2023-06-20 09:50:05</Text>
-        </Group>
-        <Title order={3}>Tranquility</Title>
-        <Group position="apart">
-          <Text>Players Online</Text>
-          <Group>
-            {tqStatus?.data.vip && <Badge>VIP Mode</Badge>}
-            <Text>{tqStatus?.data.players}</Text>
+          <Group position="apart">
+            <Text>SDE API Last Updated On</Text>
+            {/* FIXME: shouldnt be hardcoded! :) */}
+            <Text>2023-06-20 09:50:05</Text>
           </Group>
-        </Group>
-        <Group position="apart">
-          <Text>Start Time</Text>
-          <Text>
-            {tqStatus && (
-              <FormattedDateText date={new Date(tqStatus?.data.start_time)} />
-            )}
-          </Text>
-        </Group>
-        <Group position="apart">
-          <Text>Server Version</Text>
-          <Text>{tqStatus?.data.server_version}</Text>
-        </Group>
-        <Title order={3}>Static Data Export</Title>
-        <Group position="apart">
-          <Text>Last updated on</Text>
-          <Text>
-            {sdeIsLoading && "Checking..."}
-            {!sdeIsLoading && <FormattedDateText date={sdeLastModifiedDate} />}
-          </Text>
-        </Group>
-        <Title order={3}>EVE Swagger Interface</Title>
-        <Group position="apart">
-          <Text>Degraded Endpoints</Text>
-          <Text>{nonGreenEndpoints?.length}</Text>
-        </Group>
-        {sortedEsiStatusTags.map((tag) => (
-          <div key={tag}>
-            <Title order={6}>{tag}</Title>
-            <Table verticalSpacing={4} horizontalSpacing={4} fontSize="xs">
-              <tbody>
-                {esiStatusByTag[tag]?.map((entry) => (
-                  <tr key={`${entry.method} ${entry.route}`}>
-                    <td width={1}>{entry.method.toUpperCase()}</td>
-                    <td>{entry.route}</td>
-                    <td align="right">{entry.endpoint}</td>
-                    <td align="right" width={1}>
-                      <ColorSwatch size={16} color={entry.status} />
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </Table>
-          </div>
-        ))}
+        </Stack>
+        <Stack spacing="xs">
+          <Title order={3}>EVE Online</Title>
+          <Group position="apart">
+            <Text>Tranquility - Players Online</Text>
+            <Group>
+              {tqStatus?.data.vip && <Badge>VIP Mode</Badge>}
+              <Text>{tqStatus?.data.players.toLocaleString()}</Text>
+            </Group>
+          </Group>
+          <Group position="apart">
+            <Text>Start Time</Text>
+            <Text>
+              {tqStatus && (
+                <FormattedDateText date={new Date(tqStatus?.data.start_time)} />
+              )}
+            </Text>
+          </Group>
+          <Group position="apart">
+            <Text>Server Version</Text>
+            <Text>{tqStatus?.data.server_version}</Text>
+          </Group>
+          <Group position="apart">
+            <Text>SDE Last Updated On</Text>
+            <Text>
+              {sdeIsLoading && "Checking..."}
+              {!sdeIsLoading && (
+                <FormattedDateText date={sdeLastModifiedDate} />
+              )}
+            </Text>
+          </Group>
+          <Group position="apart">
+            <Text>ESI Degraded Endpoints</Text>
+            <Text>
+              {nonGreenEndpoints?.length === 0
+                ? "None"
+                : nonGreenEndpoints.length}
+            </Text>
+          </Group>
+          {sortedEsiStatusTags.map((tag) => (
+            <div key={tag}>
+              <Title order={6}>{tag}</Title>
+              <Table verticalSpacing={4} horizontalSpacing={4} fontSize="xs">
+                <tbody>
+                  {esiStatusByTag[tag]?.map((entry) => (
+                    <tr key={`${entry.method} ${entry.route}`}>
+                      <td width={1}>{entry.method.toUpperCase()}</td>
+                      <td>{entry.route}</td>
+                      <td align="right">{entry.endpoint}</td>
+                      <td align="right" width={1}>
+                        <ColorSwatch size={16} color={entry.status} />
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            </div>
+          ))}
+        </Stack>
       </Stack>
     </Container>
   );
