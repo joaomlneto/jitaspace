c1f0973b9c81920a84857e177e24c0a98c3f63ec
João Neto <joao@neto.pt>
2023-09-14 16:41:25 +0200

    web: generate sitemap after building

---
+++ apps/web/next-sitemap.config.js
@@ -0,0 +1,11 @@
+/**
+ * Configuration for `next-sitemap`
+ * @see https://www.npmjs.com/package/next-sitemap
+ */
+
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.jita.space",
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
