020f13d873c87b3c109d929907b5434f2e18785c
João Neto <joao@neto.pt>
2023-09-14 14:18:45 +0200

    web: add AssetsTable component

---
+++ apps/web/components/Assets/AssetsTable.tsx
@@ -0,0 +1,73 @@
+import React, { memo } from "react";
+import { Badge, Group, Table, Text } from "@mantine/core";
+
+import { GetCharactersCharacterIdAssets200Item } from "@jitaspace/esi-client";
+import {
+  EveEntityAnchor,
+  EveEntityName,
+  ISKAmount,
+  TypeAnchor,
+  TypeAvatar,
+  TypeName,
+} from "@jitaspace/ui";
+
+type AssetsTableProps = {
+  assets: (GetCharactersCharacterIdAssets200Item & {
+    name?: string;
+    price?: number;
+  })[];
+};
+
+export const AssetsTable = memo(({ assets }: AssetsTableProps) => {
+  return (
+    <Table highlightOnHover>
+      <thead>
+        <tr>
+          <th>Item ID</th>
+          <th>Qty</th>
+          <th>Type</th>
+          <th>Price</th>
+          <th>Location</th>
+        </tr>
+      </thead>
+      <tbody>
+        {assets.map((asset) => (
+          <tr key={asset.item_id}>
+            <td>
+              <Text size="xs" color="dimmed">
+                {asset.item_id}
+              </Text>
+            </td>
+            <td align="right">{asset.quantity}</td>
+            <td>
+              <Group spacing="xs" position="apart">
+                <Group noWrap spacing="xs">
+                  <TypeAvatar size="xs" typeId={asset.type_id} />
+                  <TypeAnchor typeId={asset.type_id}>
+                    <TypeName typeId={asset.type_id} />
+                  </TypeAnchor>
+                </Group>
+                <Group spacing="xs" position="right">
+                  {asset.is_singleton && <Badge size="xs">assembled</Badge>}
+                  {asset.is_blueprint_copy && <Badge size="xs">BPC</Badge>}
+                </Group>
+              </Group>
+            </td>
+            <td>
+              {asset.price && <ISKAmount align="right" amount={asset.price} />}
+            </td>
+
+            <td>
+              <Group spacing="xs">
+                <EveEntityAnchor entityId={asset.location_id}>
+                  <EveEntityName entityId={asset.location_id} />
+                </EveEntityAnchor>
+              </Group>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+});
+AssetsTable.displayName = "AssetsTable";
