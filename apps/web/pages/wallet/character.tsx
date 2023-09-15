deb46c15b6b6137cd9d5b4b55b71889f6f2cb83b
João Neto <joao@neto.pt>
2023-09-15 15:42:19 +0200

    web: create character wallet page

---
+++ apps/web/pages/wallet/character.tsx
@@ -0,0 +1,49 @@
+import React, { type ReactElement } from "react";
+import { Container, Group, Stack, Title } from "@mantine/core";
+import { NextSeo } from "next-seo";
+
+import { useGetCharactersCharacterIdWalletJournal } from "@jitaspace/esi-client";
+import { useEsiClientContext } from "@jitaspace/esi-hooks";
+import { WalletIcon } from "@jitaspace/eve-icons";
+
+import { WalletTable } from "~/components/Wallet";
+import { MainLayout } from "~/layouts";
+
+export default function Page() {
+  const { characterId, scopes, isTokenValid } = useEsiClientContext();
+  const { data } = useGetCharactersCharacterIdWalletJournal(
+    characterId ?? 0,
+    {},
+    {
+      swr: {
+        enabled:
+          isTokenValid &&
+          characterId !== undefined &&
+          scopes.includes("esi-wallet.read_character_wallet.v1"),
+      },
+    },
+  );
+
+  return (
+    <Container size="xl">
+      <Stack>
+        <Group>
+          <WalletIcon width={48} />
+          <Title>Wallet</Title>
+        </Group>
+        <WalletTable entries={data?.data ?? []} />
+      </Stack>
+    </Container>
+  );
+}
+
+Page.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <MainLayout>
+      <NextSeo title="Character Wallet" />
+      {page}
+    </MainLayout>
+  );
+};
+
+Page.requiredScopes = ["esi-wallet.read_character_wallet.v1"];
---
+++ apps/web/pages/wallet/index.tsx
@@ -0,0 +1,128 @@
+import React, { type ReactElement } from "react";
+import Link from "next/link";
+import {
+  Card,
+  Container,
+  createStyles,
+  Group,
+  rem,
+  SimpleGrid,
+  Text,
+  Title,
+  UnstyledButton,
+} from "@mantine/core";
+
+import { WalletIcon } from "@jitaspace/eve-icons";
+
+import { MainLayout } from "~/layouts";
+
+const useStyles = createStyles((theme) => ({
+  title: {
+    fontSize: rem(34),
+    fontWeight: 900,
+
+    [theme.fn.smallerThan("sm")]: {
+      fontSize: rem(24),
+    },
+  },
+
+  description: {
+    maxWidth: 600,
+    margin: "auto",
+
+    "&::after": {
+      content: '""',
+      display: "block",
+      backgroundColor: theme.fn.primaryColor(),
+      width: rem(45),
+      height: rem(2),
+      marginTop: theme.spacing.sm,
+      marginLeft: "auto",
+      marginRight: "auto",
+    },
+  },
+
+  card: {
+    minHeight: 200,
+    transition: "transform 0.2s",
+    border: `${rem(1)} solid ${
+      theme.colorScheme === "dark" ? theme.colors.dark[5] : theme.colors.gray[1]
+    }`,
+    "&:hover": {
+      transform: "scale(1.05)",
+    },
+  },
+
+  cardTitle: {
+    "&::after": {
+      content: '""',
+      display: "block",
+      backgroundColor: theme.fn.primaryColor(),
+      width: rem(45),
+      height: rem(2),
+      marginTop: theme.spacing.sm,
+    },
+  },
+}));
+
+export default function Page() {
+  const { classes, theme } = useStyles();
+
+  return (
+    <Container size="lg">
+      <Group>
+        <WalletIcon width={48} />
+        <Title order={1}>Assets</Title>
+      </Group>
+      <SimpleGrid
+        cols={2}
+        spacing="xl"
+        my="xl"
+        breakpoints={[{ maxWidth: "md", cols: 1 }]}
+      >
+        <UnstyledButton component={Link} href="/wallet/character">
+          <Card shadow="md" radius="md" className={classes.card} padding="xl">
+            <Container m={0} p={0} w={64} h={64}>
+              <WalletIcon
+                height={64}
+                width={64}
+                color={theme.fn.primaryColor()}
+              />
+            </Container>
+            <Group>
+              <Text fz="lg" fw={500} className={classes.cardTitle} mt="md">
+                Character Wallet
+              </Text>
+            </Group>
+            <Text fz="sm" c="dimmed" mt="sm">
+              View your character wallet.
+            </Text>
+          </Card>
+        </UnstyledButton>
+        <UnstyledButton component={Link} href="/wallet/corporation">
+          <Card shadow="md" radius="md" className={classes.card} padding="xl">
+            <Container m={0} p={0} w={64} h={64}>
+              <WalletIcon
+                height={64}
+                width={64}
+                color={theme.fn.primaryColor()}
+              />
+            </Container>
+            <Group>
+              <Text fz="lg" fw={500} className={classes.cardTitle} mt="md">
+                Corporation Assets
+              </Text>
+            </Group>
+            <Text fz="sm" c="dimmed" mt="sm">
+              View your corporation wallet.
+            </Text>
+          </Card>
+        </UnstyledButton>
+      </SimpleGrid>
+    </Container>
+  );
+}
+
+Page.getLayout = function getLayout(page: ReactElement) {
+  return <MainLayout>{page}</MainLayout>;
+};
