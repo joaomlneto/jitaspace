ceb788880b3f0a30c1e85b7da3353a4cfa354308
João Neto <joao@neto.pt>
2023-09-18 11:53:23 +0200

    web: sitemap: generate entries for type pages

--- apps/web/next-sitemap.config.js
+++ apps/web/next-sitemap.config.js
@@ -7,5 +7,27 @@
 module.exports = {
   siteUrl: "https://www.jita.space",
   generateRobotsTxt: true, // (optional)
+  additionalPaths: async () => {
+    /** @type {{loc: string}[]} */
+    const paths = [];
+
+    // Add pages for Types
+    const firstPage = await fetch(
+      `https://esi.evetech.net/latest/universe/types/`,
+    );
+    /** @type number[] */
+    const numPages = firstPage.headers.get("x-pages");
+    let typeIds = await firstPage.json();
+    for (let page = 2; page <= numPages; page++) {
+      // FIXME: GET PATH FROM SPEC
+      const pageTypeIds = await fetch(
+        `https://esi.evetech.net/latest/universe/types/?page=${page}`,
+      ).then((res) => res.json());
+      typeIds = [...typeIds, ...pageTypeIds];
+    }
+    paths.push(...typeIds.map((typeId) => ({ loc: `/type/${typeId}` })));
+
+    return paths;
+  },
   // ...other options
 };
