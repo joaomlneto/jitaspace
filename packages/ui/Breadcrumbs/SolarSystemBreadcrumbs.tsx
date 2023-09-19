9c287b081a573dcb8f61ee96faaf957dfa0bb2be
João Neto <joao@neto.pt>
2023-09-19 13:32:19 +0200

    ui: load breadcrumbs directly from ESI

--- packages/ui/Breadcrumbs/SolarSystemBreadcrumbs.tsx
+++ packages/ui/Breadcrumbs/SolarSystemBreadcrumbs.tsx
@@ -1,7 +1,10 @@
 import React, { memo } from "react";
 import { Breadcrumbs, type BreadcrumbsProps } from "@mantine/core";
 
-import { useGetSolarSystemById } from "@jitaspace/sde-client";
+import {
+  useGetUniverseConstellationsConstellationId,
+  useGetUniverseSystemsSystemId,
+} from "@jitaspace/esi-client";
 
 import {
   ConstellationAnchor,
@@ -16,21 +19,35 @@ export type SolarSystemBreadcrumbsProps = Omit<BreadcrumbsProps, "children"> & {
 
 export const SolarSystemBreadcrumbs = memo(
   ({ solarSystemId, ...otherProps }: SolarSystemBreadcrumbsProps) => {
-    const { data } = useGetSolarSystemById(
+    const { data: solarSystem } = useGetUniverseSystemsSystemId(
       typeof solarSystemId === "string"
         ? parseInt(solarSystemId)
         : solarSystemId ?? 0,
+      {},
       {
-        query: { enabled: solarSystemId !== undefined },
+        swr: { enabled: solarSystemId !== undefined },
+      },
+    );
+    const { data: constellation } = useGetUniverseConstellationsConstellationId(
+      solarSystem?.data.constellation_id ?? 0,
+      {},
+      {
+        swr: {
+          enabled: solarSystem?.data.constellation_id !== undefined,
+        },
       },
     );
     return (
       <Breadcrumbs {...otherProps}>
-        <RegionAnchor regionId={data?.regionID}>
-          <RegionName regionId={data?.regionID} />
+        <RegionAnchor regionId={constellation?.data.region_id}>
+          <RegionName regionId={constellation?.data.region_id} />
         </RegionAnchor>
-        <ConstellationAnchor constellationId={data?.constellationID}>
-          <ConstellationName constellationId={data?.constellationID} />
+        <ConstellationAnchor
+          constellationId={solarSystem?.data.constellation_id}
+        >
+          <ConstellationName
+            constellationId={solarSystem?.data.constellation_id}
+          />
         </ConstellationAnchor>
         <SolarSystemAnchor solarSystemId={solarSystemId}>
           <SolarSystemName solarSystemId={solarSystemId} />
