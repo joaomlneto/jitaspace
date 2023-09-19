6e784bfcbf1612558a5484c3d9dee824f48f7167
João Neto <joao@neto.pt>
2023-09-19 23:10:31 +0200

    migrate RequestPermissionsBanner

--- apps/web/components/ScopeGuard/RequestPermissionsBanner.tsx
+++ apps/web/components/ScopeGuard/RequestPermissionsBanner.tsx
@@ -4,7 +4,6 @@ import {
   Center,
   Collapse,
   Container,
-  createStyles,
   Group,
   Text,
   Title,
@@ -17,48 +16,8 @@ import { type ESIScope } from "@jitaspace/esi-client";
 import { useEsiClientContext } from "@jitaspace/esi-hooks";
 import { LoginWithEveOnlineButton } from "@jitaspace/ui";
 
+import classes from "./RequestPermissionsBanner.module.css";
 import { ScopesTable } from "./ScopesTable";
-
-const useStyles = createStyles((theme) => ({
-  root: {
-    paddingTop: 80,
-    paddingBottom: 80,
-  },
-
-  label: {
-    textAlign: "center",
-    fontWeight: 900,
-    fontSize: 220,
-    lineHeight: 1,
-    marginBottom: theme.spacing.xl,
-    color:
-      theme.colorScheme === "dark"
-        ? theme.colors.dark[4]
-        : theme.colors.gray[2],
-
-    [theme.fn.smallerThan("sm")]: {
-      fontSize: 120,
-    },
-  },
-
-  title: {
-    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-    textAlign: "center",
-    fontWeight: 900,
-    fontSize: 38,
-
-    [theme.fn.smallerThan("sm")]: {
-      fontSize: 32,
-    },
-  },
-
-  description: {
-    maxWidth: 600,
-    margin: "auto",
-    marginTop: theme.spacing.xl,
-    marginBottom: theme.spacing.xl,
-  },
-}));
 
 export type RequestPermissionsBannerProps = {
   requiredScopes: ESIScope[];
@@ -67,7 +26,6 @@ export type RequestPermissionsBannerProps = {
 export function RequestPermissionsBanner({
   requiredScopes,
 }: RequestPermissionsBannerProps) {
-  const { classes } = useStyles();
   const { scopes: grantedScopes } = useEsiClientContext();
 
   const [openGrantedScopesTable, { toggle: toggleGrantedScopesTable }] =
@@ -80,12 +38,7 @@ export function RequestPermissionsBanner({
   return (
     <Container className={classes.root}>
       <Title className={classes.title}>Insufficient Scopes</Title>
-      <Text
-        color="dimmed"
-        size="md"
-        align="center"
-        className={classes.description}
-      >
+      <Text c="dimmed" size="md" ta="center" className={classes.description}>
         We must request additional scopes to continue.
         <br />
         Click the login button to request them.
@@ -124,7 +77,7 @@ export function RequestPermissionsBanner({
           Customize
         </Button>
       </Group>
-      <Title order={4} align="center" mt="xl">
+      <Title order={4} ta="center" mt="xl">
         Missing Scopes
       </Title>
       <ScopesTable scopes={missingScopes} />
