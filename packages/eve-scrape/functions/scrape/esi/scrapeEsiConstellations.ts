9d41e31651f5a2a26e75cf7b7e69aed4cc1e5c1b
João Neto <joao@neto.pt>
2023-09-21 22:30:53 +0200

    eve-scrape: migrate scrapeEsiConstellations

--- packages/eve-scrape/functions/scrape/esi/scrapeEsiConstellations.ts
+++ packages/eve-scrape/functions/scrape/esi/scrapeEsiConstellations.ts
@@ -1,11 +1,14 @@
-import { Constellation, prisma } from "@jitaspace/db";
+import axios from "axios";
+import pLimit from "p-limit";
+
+import { prisma } from "@jitaspace/db";
 import {
   getUniverseConstellations,
   getUniverseConstellationsConstellationId,
-  GetUniverseConstellationsConstellationId200,
 } from "@jitaspace/esi-client";
 
 import { inngest } from "../../../client";
+import { excludeObjectKeys, updateTable } from "../../../utils";
 
 export type ScrapeConstellationEventPayload = {
   data: {};
@@ -14,106 +17,88 @@ export type ScrapeConstellationEventPayload = {
 export const scrapeEsiConstellations = inngest.createFunction(
   { name: "Scrape Constellations" },
   { event: "scrape/esi/constellations" },
-  async ({ event, step, logger }) => {
+  async ({}) => {
+    const stepStartTime = performance.now();
+    // FIXME: THIS SHOULD NOT BE NECESSARY
+    axios.defaults.baseURL = "https://esi.evetech.net/latest";
+
     // Get all Constellation IDs in ESI
-    const { data: constellationIds } = await getUniverseConstellations();
-    logger.info(`going to fetch ${constellationIds.length} constellations`);
-
-    // Get all Constellation details from ESI
-    const fetchESIDetailsStartTime = performance.now();
-    const constellationResponses = await Promise.all(
-      constellationIds.map(async (constellationId) =>
-        getUniverseConstellationsConstellationId(constellationId),
-      ),
+    const constellationIds = await getUniverseConstellations().then(
+      (res) => res.data,
     );
-    logger.info(
-      `fetched ESI constellations in ${
-        performance.now() - fetchESIDetailsStartTime
-      }`,
-    );
+    constellationIds.sort((a, b) => a - b);
 
-    // extract bodies
-    const constellations = constellationResponses.map((res) => res.data);
+    const limit = pLimit(20);
 
-    // determine which records to be created/updated/removed
-    const existingIdsInDb = await prisma.constellation
-      .findMany({
-        select: {
-          constellationId: true,
-        },
-      })
-      .then((constellations) =>
-        constellations.map((constellation) => constellation.constellationId),
-      );
-
-    const recordsToCreate = constellations.filter(
-      (constellation) =>
-        !existingIdsInDb.includes(constellation.constellation_id),
-    );
-    const recordsToUpdate = constellations.filter((constellation) =>
-      existingIdsInDb.includes(constellation.constellation_id),
-    );
-    const recordsToDelete = existingIdsInDb.filter(
-      (constellationId) => !constellationIds.includes(constellationId),
-    );
-
-    logger.info("records to create:", recordsToCreate.length);
-    logger.info("records to update:", recordsToUpdate.length);
-    logger.info("records to delete:", recordsToDelete.length);
-
-    const fromEsiToSchema = (
-      constellation: GetUniverseConstellationsConstellationId200,
-    ): Omit<Constellation, "updatedAt"> => ({
-      constellationId: constellation.constellation_id,
-      name: constellation.name,
-      regionId: constellation.region_id,
-      isDeleted: false,
-    });
-
-    // create missing records
-    const createRecordsStartTime = performance.now();
-    const createResult = await prisma.constellation.createMany({
-      data: recordsToCreate.map(fromEsiToSchema),
-      skipDuplicates: true,
-    });
-    logger.info(
-      `created records in ${performance.now() - createRecordsStartTime}ms`,
-    );
-
-    // update all records with new data
-    const updateRecordsStartTime = performance.now();
-    const updateResult = await Promise.all(
-      recordsToUpdate.map((constellation) =>
-        prisma.constellation.update({
-          data: fromEsiToSchema(constellation),
-          where: { constellationId: constellation.constellation_id },
+    const constellationChanges = await updateTable({
+      fetchLocalEntries: async () =>
+        prisma.constellation
+          .findMany({
+            where: {
+              constellationId: {
+                in: constellationIds,
+              },
+            },
+          })
+          .then((entries) =>
+            entries.map((entry) => excludeObjectKeys(entry, ["updatedAt"])),
+          ),
+      fetchRemoteEntries: async () =>
+        Promise.all(
+          constellationIds.map((constellationId) =>
+            limit(async () =>
+              getUniverseConstellationsConstellationId(constellationId)
+                .then((res) => res.data)
+                .then((constellation) => ({
+                  constellationId: constellation.constellation_id,
+                  name: constellation.name,
+                  regionId: constellation.region_id,
+                  isDeleted: false,
+                })),
+            ),
+          ),
+        ),
+      batchCreate: (entries) =>
+        limit(() =>
+          prisma.constellation.createMany({
+            data: entries,
+          }),
+        ),
+      batchDelete: (entries) =>
+        prisma.constellation.updateMany({
+          data: {
+            isDeleted: true,
+          },
+          where: {
+            constellationId: {
+              in: entries.map((entry) => entry.constellationId),
+            },
+          },
         }),
-      ),
-    );
-    logger.info(
-      `updated records in ${performance.now() - updateRecordsStartTime}ms`,
-    );
-
-    // mark records as deleted if missing from ESI
-    const deleteRecordsStartTime = performance.now();
-    const deleteResult = await prisma.constellation.updateMany({
-      data: {
-        isDeleted: true,
-      },
-      where: {
-        constellationId: {
-          in: recordsToDelete,
-        },
-      },
+      batchUpdate: (entries) =>
+        Promise.all(
+          entries.map((entry) =>
+            limit(async () =>
+              prisma.constellation.update({
+                data: entry,
+                where: { constellationId: entry.constellationId },
+              }),
+            ),
+          ),
+        ),
+      idAccessor: (e) => e.constellationId,
     });
-    logger.info(
-      `deleted records in ${performance.now() - deleteRecordsStartTime}ms`,
-    );
 
     return {
-      numCreated: createResult.count,
-      numUpdated: updateResult.length,
-      numDeleted: deleteResult.count,
+      stats: {
+        constellations: {
+          created: constellationChanges.created,
+          deleted: constellationChanges.deleted,
+          modified: constellationChanges.modified,
+          equal: constellationChanges.equal,
+        },
+      },
+      elapsed: performance.now() - stepStartTime,
     };
   },
 );
