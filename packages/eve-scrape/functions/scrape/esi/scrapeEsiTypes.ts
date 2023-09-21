1b15f2cf47e497162553a10bb74c9df363aedf8a
João Neto <joao@neto.pt>
2023-09-21 03:07:24 +0200

    Update scrapeEsiTypes.ts

--- packages/eve-scrape/functions/scrape/esi/scrapeEsiTypes.ts
+++ packages/eve-scrape/functions/scrape/esi/scrapeEsiTypes.ts
@@ -15,6 +15,100 @@ export type ScrapeTypesEventPayload = {
   };
 };
 
+const fromEsiToSchema = (
+  type: GetUniverseTypesTypeId200,
+): Omit<Type, "updatedAt"> => ({
+  typeId: type.type_id,
+  iconId: type.icon_id ?? null,
+  name: type.name,
+  description: type.description,
+  published: type.published,
+  capacity: type.capacity ?? null,
+  marketGroupId: type.market_group_id ?? null,
+  graphicId: type.graphic_id ?? null,
+  groupId: type.group_id,
+  mass: type.mass ?? null,
+  packagedVolume: type.packaged_volume ?? null,
+  portionSize: type.portion_size ?? null,
+  radius: type.radius ?? null,
+  volume: type.volume ?? null,
+  isDeleted: false,
+});
+
+const recordsAreEqual = <T>(a: T, b: T) =>
+  // @ts-expect-error indexing objects dangerously, but responsibly
+  Object.keys(a).every((key) => a[key] == b[key]);
+
+// Stolen from Prisma:
+// see https://www.prisma.io/docs/concepts/components/prisma-client/excluding-fields
+function excludeObjectKeys<T extends object, Key extends keyof T>(
+  record: T,
+  keys: Key[],
+): Omit<T, Key> {
+  // @ts-expect-error idk why
+  return Object.fromEntries(
+    // @ts-expect-error idk why
+    Object.entries(record).filter(([key]) => !keys.includes(key)),
+  );
+}
+
+export const compareSets = <T, Key extends keyof T>({
+  recordsBefore,
+  recordsAfter,
+  getId,
+  recordsAreEqual,
+}: {
+  recordsBefore: T[];
+  recordsAfter: T[];
+  getId: (t: T) => string | number;
+  recordsAreEqual: (a: T, b: T) => boolean;
+}) => {
+  const keysBefore = recordsBefore.map((record) => getId(record));
+  const keysAfter = recordsAfter.map((record) => getId(record));
+
+  const indexBefore: {
+    [key: string | number | symbol]: T;
+  } = {};
+  recordsBefore.forEach((record) => (indexBefore[getId(record)] = record));
+
+  const indexAfter: {
+    [key: string | number | symbol]: T;
+  } = {};
+  recordsAfter.forEach((record) => (indexAfter[getId(record)] = record));
+
+  // determine which records were created
+  const created: T[] = recordsAfter.filter(
+    (record) => !keysBefore.includes(getId(record)),
+  );
+
+  // determine which records were deleted
+  const deleted = recordsBefore.filter(
+    (record) => !keysAfter.includes(getId(record)),
+  );
+
+  // get the records that are common to both sets
+  const commonKeys = keysAfter.filter((key) => keysBefore.includes(key));
+  const commonRecordPairs = commonKeys.map((key) => ({
+    recordBefore: indexBefore[key]!,
+    recordAfter: indexAfter[key]!,
+  }));
+
+  // determine which records did not change
+  const equal = commonRecordPairs
+    .filter(({ recordBefore, recordAfter }) =>
+      recordsAreEqual(recordBefore, recordAfter),
+    )
+    .map(({ recordBefore, recordAfter }) => recordAfter);
+  const equalKeys = equal.map((record) => getId(record));
+
+  // determine which records have been modified
+  const modified = recordsAfter.filter(
+    (record) => !equalKeys.includes(getId(record)),
+  );
+
+  return { created, deleted, equal, modified };
+};
+
 export const scrapeEsiTypes = inngest.createFunction(
   { name: "Scrape Types" },
   { event: "scrape/esi/types" },
@@ -22,21 +116,18 @@ export const scrapeEsiTypes = inngest.createFunction(
     const batchSize = event.data.batchSize ?? 100;
 
     // Get all Type IDs in ESI
-    const typeIds = await step.run(
-      "Fetch number of pages and generate events",
-      async () => {
-        const firstPage = await getUniverseTypes();
-        const numPages = Number(firstPage.headers["x-pages"]);
-        let typeIds = firstPage.data;
-        for (let page = 2; page <= numPages; page++) {
-          typeIds.push(
-            ...(await getUniverseTypes({ page }).then((res) => res.data)),
-          );
-        }
-        typeIds.sort((a, b) => a - b);
-        return typeIds;
-      },
-    );
+    const typeIds = await step.run("Fetch Type IDs", async () => {
+      const firstPage = await getUniverseTypes();
+      const numPages = Number(firstPage.headers["x-pages"]);
+      let typeIds = firstPage.data;
+      for (let page = 2; page <= numPages; page++) {
+        typeIds.push(
+          ...(await getUniverseTypes({ page }).then((res) => res.data)),
+        );
+      }
+      typeIds.sort((a, b) => a - b);
+      return typeIds;
+    });
 
     await step.run("Mark deleted types as such", async () => {
       const result = await prisma.type.updateMany({
@@ -66,8 +157,6 @@ export const scrapeEsiTypes = inngest.createFunction(
       const stepName = `Batch ${
         i + 1
       }/${numBatches}: typeIds [${thisBatchFirst} - ${thisBatchLast}]`;
-      console.log("THIS BATCH TYPE IDs", thisBatchTypeIds);
-      console.log("THIS BATCH TYPE IDs LENGTH", thisBatchTypeIds.length);
       await step.run(stepName, async () => {
         // Get page Types' details from ESI
         logger.info(
@@ -83,230 +172,187 @@ export const scrapeEsiTypes = inngest.createFunction(
             performance.now() - fetchESIDetailsStartTime
           }`,
         );
-
-        // extract bodies
         const types = typeResponses.map((res) => res.data);
+        const convertedTypes = types.map((type) => fromEsiToSchema(type));
 
         // determine which records to be created/updated/removed
-        const existingIdsInDb = await prisma.type
-          .findMany({
-            select: {
-              typeId: true,
+        // first, get the relevant records from the database
+        const existingRecordsInDb = await prisma.type.findMany({
+          where: {
+            typeId: {
+              in: thisBatchTypeIds,
             },
-          })
-          .then((types) => types.map((type) => type.typeId));
-
-        const recordsToCreate = types.filter(
-          (type) => !existingIdsInDb.includes(type.type_id),
-        );
-        const recordsToUpdate = types.filter((type) =>
-          existingIdsInDb.includes(type.type_id),
-        );
-
-        logger.info("records to create:", recordsToCreate.length);
-        logger.info("records to update:", recordsToUpdate.length);
-
-        const fromEsiToSchema = (
-          type: GetUniverseTypesTypeId200,
-        ): Omit<Type, "updatedAt"> => ({
-          typeId: type.type_id,
-          iconId: type.icon_id ?? null,
-          name: type.name,
-          description: type.description,
-          published: type.published,
-          capacity: type.capacity ?? null,
-          marketGroupId: type.market_group_id ?? null,
-          graphicId: type.graphic_id ?? null,
-          groupId: type.group_id,
-          mass: type.mass ?? null,
-          packagedVolume: type.packaged_volume ?? null,
-          portionSize: type.portion_size ?? null,
-          radius: type.radius ?? null,
-          volume: type.volume ?? null,
-          isDeleted: false,
+          },
         });
 
-        // create missing records
-        const createRecordsStartTime = performance.now();
+        const typeChanges = compareSets({
+          recordsBefore: existingRecordsInDb.map((record) =>
+            excludeObjectKeys(record, ["updatedAt"]),
+          ),
+          recordsAfter: convertedTypes,
+          getId: (t) => t.typeId,
+          recordsAreEqual,
+        });
         const createResult = await prisma.type.createMany({
-          data: recordsToCreate.map(fromEsiToSchema),
-          skipDuplicates: true,
+          data: typeChanges.created,
         });
-        logger.info(
-          `created records in ${performance.now() - createRecordsStartTime}ms`,
-        );
-
-        // update all records with new data
-        const updateRecordsStartTime = performance.now();
         const updateResult = await Promise.all(
-          recordsToUpdate.map((type) =>
+          typeChanges.modified.map((type) =>
             limit(async () =>
               prisma.type.update({
-                data: { ...fromEsiToSchema(type) },
-                where: { typeId: type.type_id },
+                data: type,
+                where: { typeId: type.typeId },
               }),
             ),
           ),
         );
-        logger.info(
-          `updated records in ${performance.now() - updateRecordsStartTime}ms`,
-        );
+        const deleteResult = await prisma.type.updateMany({
+          data: {
+            isDeleted: true,
+          },
+          where: {
+            typeId: {
+              notIn: typeChanges.deleted.map((type) => type.typeId),
+            },
+          },
+        });
 
         /**
          * Deal with DogmaAttributes attached to the Type
          */
-
-        // delete attributes that no longer exist
-        const deleteAttributesStartTime = performance.now();
+        const existingTypeAttributesInDb = await prisma.typeAttribute.findMany({
+          where: {
+            typeId: {
+              in: thisBatchTypeIds,
+            },
+          },
+        });
+        const typeAttributesChanges = compareSets({
+          recordsBefore: existingTypeAttributesInDb.map((record) =>
+            excludeObjectKeys(record, ["updatedAt", "isDeleted"]),
+          ),
+          recordsAfter: types.flatMap((type) =>
+            (type.dogma_attributes ?? []).map((typeAttribute) => ({
+              attributeId: typeAttribute.attribute_id,
+              typeId: type.type_id,
+              value: typeAttribute.value,
+            })),
+          ),
+          getId: (t) => `${t.typeId}:${t.attributeId}`,
+          recordsAreEqual,
+        });
+        // XXX: We cannot batch these updates because we have a composite ID on this table
         const deleteAttributesResult = await Promise.all(
-          types.map((type) =>
+          typeAttributesChanges.deleted.map((typeAttribute) =>
             limit(async () =>
-              prisma.typeAttribute.updateMany({
+              prisma.typeAttribute.update({
                 data: { isDeleted: true },
                 where: {
-                  typeId: type.type_id,
-                  attributeId: {
-                    notIn: (type.dogma_attributes ?? []).map(
-                      (typeAttributes) => typeAttributes.attribute_id,
-                    ),
+                  typeId_attributeId: {
+                    typeId: typeAttribute.typeId,
+                    attributeId: typeAttribute.attributeId,
                   },
                 },
               }),
             ),
           ),
         );
-        const numDeletedAttributes = deleteAttributesResult
-          .map(({ count }) => count)
-          .reduce((acc, cur) => acc + cur, 0);
-        logger.info(
-          `deleted type attributes in ${
-            performance.now() - deleteAttributesStartTime
-          }ms`,
-        );
 
         // update type attributes with new information
-        const updateAttributesStartTime = performance.now();
         const updateAttributesResult = await Promise.all(
-          types.map((type) =>
-            Promise.all(
-              (type.dogma_attributes ?? []).map((typeAttribute) =>
-                limit(async () => {
-                  return prisma.typeAttribute.upsert({
-                    update: {
-                      value: typeAttribute.value,
-                      isDeleted: false,
-                    },
-                    where: {
-                      typeId_attributeId: {
-                        typeId: type.type_id,
-                        attributeId: typeAttribute.attribute_id,
-                      },
-                      typeId: type.type_id,
-                      attributeId: typeAttribute.attribute_id,
-                    },
-                    create: {
-                      typeId: type.type_id,
-                      attributeId: typeAttribute.attribute_id,
-                      value: typeAttribute.value,
-                      isDeleted: false,
-                    },
-                  });
-                }),
-              ),
+          typeAttributesChanges.modified.map((typeAttribute) =>
+            limit(async () =>
+              prisma.typeAttribute.update({
+                data: typeAttribute,
+                where: {
+                  typeId_attributeId: {
+                    typeId: typeAttribute.typeId,
+                    attributeId: typeAttribute.attributeId,
+                  },
+                },
+              }),
             ),
           ),
-        );
-        logger.info(
-          `updated type attributes in ${
-            performance.now() - updateAttributesStartTime
-          }ms`,
-        );
-        const numUpdatedAttributes = updateAttributesResult.reduce(
-          (acc, arr) => acc + arr.length,
-          0,
         );
 
         /**
          * Deal with DogmaEffects attached to the Type
          */
-
+        const existingTypeEffectsInDb = await prisma.typeEffect.findMany({
+          where: {
+            typeId: {
+              in: thisBatchTypeIds,
+            },
+          },
+        });
+        const typeEffectsChanges = compareSets({
+          recordsBefore: existingTypeEffectsInDb.map((record) =>
+            excludeObjectKeys(record, ["updatedAt", "isDeleted"]),
+          ),
+          recordsAfter: types.flatMap((type) =>
+            (type.dogma_effects ?? []).map((typeAttribute) => ({
+              effectId: typeAttribute.effect_id,
+              typeId: type.type_id,
+              isDefault: typeAttribute.is_default,
+            })),
+          ),
+          getId: (t) => `${t.typeId}:${t.effectId}`,
+          recordsAreEqual,
+        });
         // delete effects that no longer exist
-        const deleteEffectsStartTime = performance.now();
         const deleteEffectsResult = await Promise.all(
-          types.map((type) =>
+          typeEffectsChanges.deleted.map((typeEffect) =>
             limit(async () =>
-              prisma.typeEffect.updateMany({
+              prisma.typeEffect.update({
                 data: { isDeleted: true },
                 where: {
-                  typeId: type.type_id,
-                  effectId: {
-                    notIn: (type.dogma_effects ?? []).map(
-                      (typeEffects) => typeEffects.effect_id,
-                    ),
+                  typeId_effectId: {
+                    typeId: typeEffect.typeId,
+                    effectId: typeEffect.effectId,
                   },
                 },
               }),
             ),
           ),
         );
-        const numDeletedEffects = deleteEffectsResult
-          .map(({ count }) => count)
-          .reduce((acc, cur) => acc + cur, 0);
-        logger.info(
-          `deleted type effects in ${
-            performance.now() - deleteEffectsStartTime
-          }ms`,
-        );
 
         // update type effects with new information
-        const updateEffectsStartTime = performance.now();
         const updateEffectsResult = await Promise.all(
-          types.map((type) =>
-            Promise.all(
-              (type.dogma_effects ?? []).map((typeEffect) =>
-                limit(async () => {
-                  return prisma.typeEffect.upsert({
-                    update: {
-                      isDefault: typeEffect.is_default,
-                      isDeleted: false,
-                    },
-                    where: {
-                      typeId_effectId: {
-                        typeId: type.type_id,
-                        effectId: typeEffect.effect_id,
-                      },
-                      typeId: type.type_id,
-                      effectId: typeEffect.effect_id,
-                    },
-                    create: {
-                      typeId: type.type_id,
-                      effectId: typeEffect.effect_id,
-                      isDefault: typeEffect.is_default,
-                      isDeleted: false,
-                    },
-                  });
-                }),
-              ),
+          typeEffectsChanges.modified.map((typeEffect) =>
+            limit(async () =>
+              prisma.typeEffect.update({
+                data: typeEffect,
+                where: {
+                  typeId_effectId: {
+                    typeId: typeEffect.typeId,
+                    effectId: typeEffect.effectId,
+                  },
+                },
+              }),
             ),
           ),
         );
-        logger.info(
-          `updated type effects in ${
-            performance.now() - updateEffectsStartTime
-          }ms`,
-        );
-        const numUpdatedEffects = updateEffectsResult.reduce(
-          (acc, arr) => acc + arr.length,
-          0,
-        );
 
         return {
-          numTypesCreated: createResult.count,
-          numTypesUpdated: updateResult.flat().length,
-          numUpdatedAttributes,
-          numDeletedAttributes,
-          numUpdatedEffects,
-          numDeletedEffects,
+          types: {
+            created: typeChanges.created.length,
+            deleted: typeChanges.deleted.length,
+            modified: typeChanges.modified.length,
+            equal: typeChanges.equal.length,
+          },
+          typeAttributes: {
+            created: typeAttributesChanges.created.length,
+            deleted: typeAttributesChanges.deleted.length,
+            modified: typeAttributesChanges.modified.length,
+            equal: typeAttributesChanges.equal.length,
+          },
+          typeEffects: {
+            created: typeEffectsChanges.created.length,
+            deleted: typeEffectsChanges.deleted.length,
+            modified: typeEffectsChanges.modified.length,
+            equal: typeEffectsChanges.equal.length,
+          },
+          elapsed: performance.now() - fetchESIDetailsStartTime,
         };
       });
     }
