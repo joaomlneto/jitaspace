a5b1dabd1f4ce8dd48caf72e604f954f32819eda
João Neto <joao@neto.pt>
2023-09-22 15:30:10 +0200

    eve-scrape: fix comparison of records

--- packages/eve-scrape/utils/recordsAreEqual.ts
+++ packages/eve-scrape/utils/recordsAreEqual.ts
@@ -1,3 +1,5 @@
+import Decimal from "decimal.js";
+
 export const recordsAreEqual = <
   T extends Record<string | number | symbol, any>,
 >(
@@ -6,7 +8,36 @@ export const recordsAreEqual = <
   opts?: {
     ignoreKeys?: (keyof T)[];
   },
-) =>
+): boolean =>
   Object.keys(a)
     .filter((key) => !(opts?.ignoreKeys ?? []).includes(key as keyof T))
-    .every((key) => a[key] == b[key]);
+    .every((key) => {
+      // different types? this shouldn't happen
+      if (typeof a[key] !== typeof b[key]) return false;
+
+      // check if we are comparing Decimals (decimal.js)
+      if (Decimal.isDecimal(a[key])) {
+        //console.log("decimal", a[key], b[key]);
+        return a[key].comparedTo(b[key]) == 0;
+      }
+
+      // check if we are dealing with null values
+      // typeof returns "object" because javascript
+      if (a[key] === null || b[key] === null) {
+        return a[key] === null && b[key] === null;
+      }
+
+      // check if we are comparing objects
+      if (typeof a[key] == "object") {
+        //console.log("object", a[key], b[key]);
+        return recordsAreEqual(a[key], b[key], opts);
+      }
+
+      // check if we are comparing floats
+      if (typeof a[key] == "number" && (a[key] % 1 !== 0 || b[key] % 1 !== 0)) {
+        //console.log("number", a[key], b[key]);
+        return a[key].toString() == b[key].toString();
+      }
+
+      return a[key] == b[key];
+    });
