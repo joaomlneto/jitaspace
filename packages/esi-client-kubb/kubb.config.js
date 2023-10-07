91d38feaaa62bee444921aa25e1f83e4bb2e3102
João Neto <1539767+joaomlneto@users.noreply.github.com>
2023-10-07 22:42:40 +0200

    kubb: generate react-query-infinite hooks

--- packages/esi-client-kubb/kubb.config.js
+++ packages/esi-client-kubb/kubb.config.js
@@ -24,6 +24,7 @@ export default defineConfig(async () => {
       createSwaggerTS({}),
       createSwaggerTanstackQuery({
         //client: "./src/client",
+        infinite: {},
       }),
       createSwaggerZod({}),
       createSwaggerZodios({}),
