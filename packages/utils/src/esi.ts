685714cd8aa4306e34aa30210171f33740ebc0b6
João Neto <1539767+joaomlneto@users.noreply.github.com>
2023-10-07 20:52:50 +0200

    Update esi.ts

--- packages/utils/src/esi.ts
+++ packages/utils/src/esi.ts
@@ -1,13 +1,12 @@
-import { type GetCharactersCharacterIdMailLabels200LabelsItem } from "@jitaspace/esi-client-kubb";
-
 export const SPECIAL_LABEL_IDS = [1, 2, 4, 8];
 
 export const isSpecialLabelId = (id?: number) =>
   id && SPECIAL_LABEL_IDS.includes(id);
 
-export const humanLabelName = (
-  label?: GetCharactersCharacterIdMailLabels200LabelsItem,
-): string => {
+export const humanLabelName = (label?: {
+  label_id?: number;
+  name?: string;
+}): string => {
   switch (label?.label_id) {
     case 1:
       return "Inbox";
