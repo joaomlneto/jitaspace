bedfc02de4a11442e92be295d5311076b8c4b076
João Neto <1539767+joaomlneto@users.noreply.github.com>
2023-10-12 22:33:29 +0200

    update tooling

--- tooling/eslint/base.js
+++ tooling/eslint/base.js
@@ -5,6 +5,7 @@ const config = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended-type-checked",
     "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:@tanstack/eslint-plugin-query/recommended",
     "prettier",
   ],
   env: {
