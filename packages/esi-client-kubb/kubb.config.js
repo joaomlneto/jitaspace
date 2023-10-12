4caf75342574f2b374195a716701295473936966
João Neto <1539767+joaomlneto@users.noreply.github.com>
2023-10-12 17:12:42 +0200

    esi-client-kubb: return full response on axios methods

--- packages/esi-client-kubb/kubb.config.js
+++ packages/esi-client-kubb/kubb.config.js
@@ -24,6 +24,7 @@ export default defineConfig(async () => {
       createSwagger({}),
       createSwaggerClient({
         //output: "./clients/axios",
+        dataReturnType: "full",
       }),
       createSwaggerTS({}),
       createSwaggerTanstackQuery({
