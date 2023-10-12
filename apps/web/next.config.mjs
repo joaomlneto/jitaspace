a8f8a466ffd69c99dc65b7e3223110744343bbc2
João Neto <1539767+joaomlneto@users.noreply.github.com>
2023-10-12 17:13:34 +0200

    re-link esi-client for schema metadata

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -9,71 +9,72 @@ import bundleAnalyzer from "@next/bundle-analyzer";
 
 /** @type {import("next").NextConfig} */
 const config = {
-  reactStrictMode: true,
+    reactStrictMode: true,
 
-  /** Enables hot reloading for local packages without a build step */
-  transpilePackages: [
-    "jotai-devtools",
-    "@jitaspace/auth",
-    "@jitaspace/db",
-    "@jitaspace/esi-client-kubb",
-    "@jitaspace/esi-hooks",
-    "@jitaspace/esi-meta-client",
-    "@jitaspace/eve-icons",
-    "@jitaspace/eve-scrape",
-    "@jitaspace/sde-client",
-    "@jitaspace/tiptap-eve",
-    "@jitaspace/ui",
-    "@jitaspace/utils",
-  ],
-
-  /** We already do linting and typechecking as separate tasks in CI */
-  eslint: { ignoreDuringBuilds: !!process.env.CI },
-  typescript: { ignoreBuildErrors: !!process.env.CI },
-
-  /** Allow images from CCP CDN */
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "web.ccpgamescdn.com",
-      },
-      {
-        protocol: "https",
-        hostname: "images.evetech.net",
-      },
+    /** Enables hot reloading for local packages without a build step */
+    transpilePackages: [
+        "jotai-devtools",
+        "@jitaspace/auth",
+        "@jitaspace/db",
+        "@jitaspace/esi-client",
+        "@jitaspace/esi-client-kubb",
+        "@jitaspace/esi-hooks",
+        "@jitaspace/esi-meta-client",
+        "@jitaspace/eve-icons",
+        "@jitaspace/eve-scrape",
+        "@jitaspace/sde-client",
+        "@jitaspace/tiptap-eve",
+        "@jitaspace/ui",
+        "@jitaspace/utils",
     ],
-  },
 
-  rewrites: async () => [
-    {
-      source: "/analytics/:match*",
-      destination: "https://analytics.umami.is/:match*", // Proxy to Umami
+    /** We already do linting and typechecking as separate tasks in CI */
+    eslint: {ignoreDuringBuilds: !!process.env.CI},
+    typescript: {ignoreBuildErrors: !!process.env.CI},
+
+    /** Allow images from CCP CDN */
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "web.ccpgamescdn.com",
+            },
+            {
+                protocol: "https",
+                hostname: "images.evetech.net",
+            },
+        ],
     },
-  ],
 
-  async redirects() {
-    return [
-      {
-        source: "/bookmarks",
-        //destination: 'https://github.com/esi/esi-issues/issues/1340',
-        destination: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-        permanent: false,
-      },
-    ];
-  },
+    rewrites: async () => [
+        {
+            source: "/analytics/:match*",
+            destination: "https://analytics.umami.is/:match*", // Proxy to Umami
+        },
+    ],
 
-  experimental: {
-    swcPlugins: [["@swc-jotai/react-refresh", {}]],
-  },
+    async redirects() {
+        return [
+            {
+                source: "/bookmarks",
+                //destination: 'https://github.com/esi/esi-issues/issues/1340',
+                destination: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+                permanent: false,
+            },
+        ];
+    },
+
+    experimental: {
+        swcPlugins: [["@swc-jotai/react-refresh", {}]],
+    },
 };
 
 const withPWA = withPWAInit({
-  dest: "public",
+    dest: "public",
 });
 
 const withBundleAnalyzer = bundleAnalyzer({
-  enabled: process.env.ANALYZE === "true",
+    enabled: process.env.ANALYZE === "true",
 });
 
 export default withBundleAnalyzer(withPWA(config));
