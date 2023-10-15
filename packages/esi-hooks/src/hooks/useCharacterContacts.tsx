14275233ffd8d9f492fa122d4fe192de0123a601
João Neto <1539767+joaomlneto@users.noreply.github.com>
2023-10-15 22:19:35 +0200

    Update useCharacterContacts.tsx

--- packages/esi-hooks/src/hooks/useCharacterContacts.tsx
+++ packages/esi-hooks/src/hooks/useCharacterContacts.tsx
@@ -4,7 +4,7 @@ import { QueryFunctionContext, QueryKey } from "@tanstack/react-query";
 import {
   getCharactersCharacterIdContacts,
   useGetCharactersCharacterIdContactsInfinite,
-  useGetCorporationsCorporationIdContactsLabels,
+  useGetCharactersCharacterIdContactsLabels,
 } from "@jitaspace/esi-client-kubb";
 
 import { useEsiClientContext } from "./useEsiClientContext";
@@ -13,7 +13,7 @@ export function useCharacterContacts() {
   const { isTokenValid, characterId, scopes, accessToken } =
     useEsiClientContext();
 
-  const { data: labels } = useGetCorporationsCorporationIdContactsLabels(
+  const { data: labels } = useGetCharactersCharacterIdContactsLabels(
     characterId ?? 0,
     {},
     {},
@@ -54,77 +54,16 @@ export function useCharacterContacts() {
       },
     );
 
+  // fetch everything immediately
   useEffect(() => {
     if (hasNextPage) fetchNextPage();
   }, [hasNextPage, fetchNextPage]);
-
-  /*
-  const { data, error, isLoading, isValidating, size, setSize, mutate } =
-    useSWRInfinite<GetCharactersCharacterIdContactsQueryResponse[], Error>(
-      function getKey(pageIndex) {
-        if (
-          !characterId ||
-          !isTokenValid ||
-          !scopes.includes("esi-characters.read_contacts.v1")
-        ) {
-          throw new Error(
-            "Insufficient permissions to read character contacts",
-          );
-        }
-
-        return () => {
-          const [endpointUrl] =
-            getCharactersCharacterIdContactsQueryKey(characterId, {page: pageIndex + 1});
-          return `${ESI_BASE_URL}${endpointUrl}?${queryParams.toString()}`;
-        };
-      },
-      (url: string) =>
-        fetch(url, {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${accessToken}`,
-          },
-        }).then((r) => {
-          const numPagesString = r.headers.get("x-pages");
-          const numPages =
-            numPagesString !== null ? parseInt(numPagesString) : undefined;
-          if (numPages && numPages !== size) {
-            //setNumPages(numPages);
-            void setSize(numPages);
-          }
-          return r.json();
-        }),
-      { revalidateAll: true },
-    );
-
-  const { data: labels } = useGetCorporationsCorporationIdContactsLabels(
-    characterId ?? 0,
-    {},
-    {
-      swr: {
-        enabled:
-          !!characterId &&
-          isTokenValid &&
-          scopes.includes("esi-characters.read_contacts.v1"),
-        revalidateOnFocus: false,
-      },
-    },
-  );
-
-  return {
-    data: data?.flat() ?? [],
-    labels: labels?.data ?? [],
-    error,
-    isLoading,
-    isValidating,
-    mutate,
-  };*/
 
   return {
     data: (data?.pages ?? []).flatMap((res) => res.data ?? []),
     labels: labels?.data ?? [],
     error,
     isLoading,
-    mutate: () => {},
+    mutate: refetch,
   };
 }
