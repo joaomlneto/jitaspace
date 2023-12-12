f69d67edabd859118b92793b55ab5ebab0c85562
João Neto <1539767+joaomlneto@users.noreply.github.com>
2023-12-12 15:15:45 +0100

    Update test.tsx

--- apps/web/pages/test.tsx
+++ apps/web/pages/test.tsx
@@ -1,12 +1,46 @@
-import React, { type ReactElement } from "react";
-import { Container, Title } from "@mantine/core";
+import React, { useMemo, type ReactElement } from "react";
+import { Container, Group, JsonInput, Text, Title } from "@mantine/core";
+import { useSession } from "next-auth/react";
+
+import { useMultipleSessions } from "@jitaspace/hooks";
+import { CharacterAvatar, TimeAgoText } from "@jitaspace/ui";
 
 import { MainLayout } from "~/layouts";
 
+
 export default function Page() {
+  const { data: session, status, update } = useSession();
+  const x = useMultipleSessions();
+  const characters = useMemo(
+    () =>
+      Object.values(x.characters ?? {}).map((char) => ({
+        ...char,
+        expirationDate: new Date(char.accessTokenExpirationDate),
+      })),
+    [x.characters],
+  );
   return (
     <Container size="lg">
       <Title order={3}>Test Page</Title>
+      <JsonInput value={JSON.stringify(x, null, 2)} autosize maxRows={40} />
+      {false && (
+        <JsonInput
+          value={JSON.stringify(session, null, 2)}
+          autosize
+          maxRows={40}
+        />
+      )}
+      {characters.map((char) => (
+        <Container key={char.characterId}>
+          <Group position="apart">
+            <Group>
+              <CharacterAvatar characterId={char.characterId} />
+              <Text>{char.accessTokenPayload.name}</Text>
+            </Group>
+            <TimeAgoText addSuffix date={char.expirationDate} />
+          </Group>
+        </Container>
+      ))}
     </Container>
   );
 }
