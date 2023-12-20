f079747cb57891c80f984dee5dfa22e7c6904c13
João Neto <1539767+joaomlneto@users.noreply.github.com>
2023-12-20 11:24:34 +0100

    web: migrate EsiClientStateCard to new auth

--- apps/web/components/EsiClient/EsiClientStateCard.tsx
+++ apps/web/components/EsiClient/EsiClientStateCard.tsx
@@ -1,91 +1,108 @@
-import React from "react";
-import { Group, Loader, Text } from "@mantine/core";
+import React, { useMemo } from "react";
+import {
+  Button,
+  CopyButton,
+  Group,
+  Spoiler,
+  Text,
+  Title,
+  Tooltip,
+} from "@mantine/core";
 
+import { useAuthStore } from "@jitaspace/hooks";
+import {
+  CharacterAvatar,
+  CharacterName,
+  FormattedDateText,
+  TimeAgoText,
+} from "@jitaspace/ui";
 
-
+import { ScopesTable } from "../ScopeGuard";
 
 
 export function EsiClientStateCard() {
-  // TODO: Migrate to new ESI React Client Library
+  const { characters } = useAuthStore();
 
-  if (true) {
-    return (
-      <Group>
-        <Loader />
-        <Text>ESI Client Loading…</Text>
-      </Group>
-    );
-  }
+  const sortedCharacters = useMemo(
+    () =>
+      Object.values(characters).sort((a, b) =>
+        a.accessTokenPayload.name.localeCompare(b.accessTokenPayload.name),
+      ),
+    [characters],
+  );
 
-  /*
-  return (
+  return sortedCharacters.map((character) => (
     <>
       <Group grow>
-        {accessToken && (
-          <CopyButton value={accessToken}>
-            {({ copied, copy }) => (
-              <Button color={copied ? "teal" : "blue"} onClick={copy}>
-                {copied ? "ESI Access Token Copied" : "Copy ESI Access Token"}
-              </Button>
-            )}
-          </CopyButton>
-        )}
-        {characterId !== undefined && (
-          <CopyButton value={characterId.toString()}>
-            {({ copied, copy }) => (
-              <Button color={copied ? "teal" : "blue"} onClick={copy}>
-                {copied ? "Character ID Copied" : "Copy Character ID"}
-              </Button>
-            )}
-          </CopyButton>
-        )}
+        <CopyButton value={character.accessToken}>
+          {({ copied, copy }) => (
+            <Button color={copied ? "teal" : "blue"} onClick={copy}>
+              {copied ? "ESI Access Token Copied" : "Copy ESI Access Token"}
+            </Button>
+          )}
+        </CopyButton>
+        <CopyButton value={character.characterId.toString()}>
+          {({ copied, copy }) => (
+            <Button color={copied ? "teal" : "blue"} onClick={copy}>
+              {copied ? "Character ID Copied" : "Copy Character ID"}
+            </Button>
+          )}
+        </CopyButton>
       </Group>
       <Group position="apart">
         <Text>Authenticated</Text>
 
-        {characterId && (
+        <Tooltip
+          color="dark"
+          label={<Text size="sm">Character ID: {character.characterId}</Text>}
+        >
+          <Group>
+            <CharacterAvatar characterId={character.characterId} size="sm" />
+
+            <CharacterName characterId={character.characterId} />
+          </Group>
+        </Tooltip>
+      </Group>
+
+      <Group position="apart">
+        <Text>Token expires</Text>
+        <Group>
           <Tooltip
             color="dark"
-            label={<Text size="sm">Character ID: {characterId}</Text>}
+            label={
+              <FormattedDateText
+                date={new Date(character.accessTokenExpirationDate)}
+              />
+            }
           >
-            <Group>
-              <CharacterAvatar characterId={characterId} size="sm" />
-
-              <CharacterName characterId={characterId} />
-            </Group>
+            <div>
+              <TimeAgoText
+                span
+                date={new Date(character.accessTokenExpirationDate)}
+                addSuffix
+              />
+            </div>
           </Tooltip>
-        )}
-        {!characterId && <Text>No</Text>}
-      </Group>
-      {tokenExpirationDate && (
-        <Group position="apart">
-          <Text>Token expires</Text>
-          <Group>
-            {tokenExpirationDate && (
-              <Tooltip
-                color="dark"
-                label={<FormattedDateText date={tokenExpirationDate} />}
-              >
-                <div>
-                  <TimeAgoText span date={tokenExpirationDate} addSuffix />
-                </div>
-              </Tooltip>
-            )}
-          </Group>
         </Group>
-      )}
-      {scopes.length > 0 && (
+      </Group>
+
+      {character.accessTokenPayload.scp.length > 0 && (
         <>
-          <Title order={6}>{scopes.length} scopes granted</Title>
+          <Title order={6}>
+            {character.accessTokenPayload.scp.length} scopes granted
+          </Title>
           <Spoiler
             maxHeight={0}
             showLabel={<Text size="sm">Show list of scopes</Text>}
             hideLabel={<Text size="sm">Hide list of scopes</Text>}
           >
-            <ScopesTable scopes={scopes} showRawScopeNames />
+            <ScopesTable
+              scopes={character.accessTokenPayload.scp}
+              showRawScopeNames
+            />
           </Spoiler>
         </>
       )}
     </>
-  );*/
+  ));
 }
