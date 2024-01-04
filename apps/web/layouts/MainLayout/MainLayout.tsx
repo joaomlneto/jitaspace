004e8f2ee3d7515e75cec21b7682b38e513fa898
João Neto <1539767+joaomlneto@users.noreply.github.com>
2024-01-04 14:47:21 +0100

    Update MainLayout.tsx

--- apps/web/layouts/MainLayout/MainLayout.tsx
+++ apps/web/layouts/MainLayout/MainLayout.tsx
@@ -1,35 +1,29 @@
 import React, { type PropsWithChildren } from "react";
-import { AppShell, createStyles, type AppShellProps } from "@mantine/core";
+import { AppShell, type AppShellProps } from "@mantine/core";
 
 import { FooterWithLinks } from "~/layouts/MainLayout/FooterWithLinks";
-import { HeaderWithMegaMenus } from "./HeaderWithMegaMenus";
 
-
-const useStyles = createStyles(() => ({
-  container: {
-    position: "relative",
-    minHeight: "100%",
-    boxSizing: "border-box",
-  },
-}));
 
 export function MainLayout({
   children,
   ...otherProps
 }: PropsWithChildren<AppShellProps>) {
-  const { classes } = useStyles();
   return (
     <AppShell
-      header={<HeaderWithMegaMenus />}
-      className={classes.container}
-      {...otherProps}
-      styles={(_theme) => ({
+      header={{ height: 60 }} // FIXME Mantine v7 migration
+      styles={{
         main: {
           margin: 0,
           paddingLeft: 0,
           paddingRight: 0,
         },
-      })}
+        root: {
+          position: "relative",
+          minHeight: "100%",
+          boxSizing: "border-box",
+        },
+      }}
+      {...otherProps}
     >
       {children}
       <FooterWithLinks />
