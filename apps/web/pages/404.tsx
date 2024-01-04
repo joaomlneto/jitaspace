4868c6c5bc8d4bf53905b8efd98ec7af534a58c2
João Neto <1539767+joaomlneto@users.noreply.github.com>
2024-01-04 14:50:43 +0100

    Update 404.tsx

--- apps/web/pages/404.tsx
+++ apps/web/pages/404.tsx
@@ -3,69 +3,72 @@ import Link from "next/link";
 import {
   Button,
   Container,
-  createStyles,
   Group,
   rem,
   Text,
   Title,
+  useMantineColorScheme,
+  useMantineTheme,
 } from "@mantine/core";
 
 import { MainLayout } from "~/layouts";
 
 
-const useStyles = createStyles((theme) => ({
-  root: {
-    paddingTop: rem(80),
-    paddingBottom: rem(80),
-  },
-
-  label: {
-    textAlign: "center",
-    fontWeight: 900,
-    fontSize: rem(220),
-    lineHeight: 1,
-    marginBottom: `calc(${theme.spacing.xl} * 1.5)`,
-    color:
-      theme.colorScheme === "dark"
-        ? theme.colors.dark[4]
-        : theme.colors.gray[2],
-
-    [theme.fn.smallerThan("sm")]: {
-      fontSize: rem(120),
-    },
-  },
-
-  title: {
-    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-    textAlign: "center",
-    fontWeight: 900,
-    fontSize: rem(38),
-
-    [theme.fn.smallerThan("sm")]: {
-      fontSize: rem(32),
-    },
-  },
-
-  description: {
-    maxWidth: rem(500),
-    margin: "auto",
-    marginTop: theme.spacing.xl,
-    marginBottom: `calc(${theme.spacing.xl} * 1.5)`,
-  },
-}));
-
 export default function Page() {
-  const { classes } = useStyles();
+  const theme = useMantineTheme();
+  const { colorScheme } = useMantineColorScheme();
 
   return (
-    <Container className={classes.root}>
-      <div className={classes.label}>404</div>
-      <Title className={classes.title}>You have found a secret place.</Title>
+    <Container
+      style={{
+        paddingTop: rem(80),
+        paddingBottom: rem(80),
+      }}
+    >
+      <div
+        style={{
+          textAlign: "center",
+          fontWeight: 900,
+          fontSize: rem(220),
+          lineHeight: 1,
+          marginBottom: `calc(${theme.spacing.xl} * 1.5)`,
+          color:
+            colorScheme === "dark"
+              ? theme.colors.dark[4]
+              : theme.colors.gray[2],
+
+          /*
+        [theme.fn.smallerThan("sm")]: {
+          fontSize: rem(120),
+        },*/ // FIXME Mantine v7 migration
+        }}
+      >
+        404
+      </div>
+      <Title
+        style={{
+          fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+          textAlign: "center",
+          fontWeight: 900,
+          fontSize: rem(38),
+          /*
+        [theme.fn.smallerThan("sm")]: {
+          fontSize: rem(32),
+        },*/ // FIXME Mantine v7 migration
+        }}
+      >
+        You have found a secret place.
+      </Title>
       <Text
         color="dimmed"
         size="lg"
         ta="center"
-        className={classes.description}
+        style={{
+          maxWidth: rem(500),
+          margin: "auto",
+          marginTop: theme.spacing.xl,
+          marginBottom: `calc(${theme.spacing.xl} * 1.5)`,
+        }}
       >
         Unfortunately, this is only a 404 page. You may have mistyped the
         address, or the page has been moved to another URL.
