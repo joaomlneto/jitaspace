4b1bc6212e2ea4da2b01ed5fb4208906c254eeb8
João Neto <1539767+joaomlneto@users.noreply.github.com>
2024-01-04 14:48:27 +0100

    Update UserButton.tsx

--- apps/web/layouts/MainLayout/UserButton.tsx
+++ apps/web/layouts/MainLayout/UserButton.tsx
@@ -1,10 +1,11 @@
 import React, { useMemo } from "react";
 import {
-  createStyles,
   Group,
   Menu,
   Text,
   UnstyledButton,
+  useMantineColorScheme,
+  useMantineTheme,
   type UnstyledButtonProps,
 } from "@mantine/core";
 import { openContextModal } from "@mantine/modals";
@@ -18,32 +19,13 @@ import {
 import { useAuthStore, useSelectedCharacter } from "@jitaspace/hooks";
 import { CharacterAvatar } from "@jitaspace/ui";
 
-
-
-
-
-const useStyles = createStyles((theme) => ({
-  user: {
-    display: "block",
-    width: "100%",
-    padding: theme.spacing.md,
-    color: theme.colorScheme === "dark" ? theme.colors.dark[0] : theme.black,
-
-    "&:hover": {
-      backgroundColor:
-        theme.colorScheme === "dark"
-          ? theme.colors.dark[6]
-          : theme.colors.gray[0],
-    },
-  },
-}));
-
 interface UserButtonProps extends UnstyledButtonProps {
   icon?: React.ReactNode;
 }
 
 export default function UserButton({ ...others }: UserButtonProps) {
-  const { classes } = useStyles();
+  const theme = useMantineTheme();
+  const { colorScheme } = useMantineColorScheme();
   const character = useSelectedCharacter();
   const { characters, selectCharacter } = useAuthStore();
 
@@ -63,7 +45,22 @@ export default function UserButton({ ...others }: UserButtonProps) {
   return (
     <Menu withArrow position="bottom" transitionProps={{ transition: "pop" }}>
       <Menu.Target>
-        <UnstyledButton className={classes.user} {...others}>
+        <UnstyledButton
+          style={{
+            display: "block",
+            width: "100%",
+            padding: theme.spacing.md,
+            color: colorScheme === "dark" ? theme.colors.dark[0] : theme.black,
+
+            "&:hover": {
+              backgroundColor:
+                colorScheme === "dark"
+                  ? theme.colors.dark[6]
+                  : theme.colors.gray[0],
+            },
+          }}
+          {...others}
+        >
           <Group>
             <CharacterAvatar characterId={characterId} radius="xl" size="sm" />
 
