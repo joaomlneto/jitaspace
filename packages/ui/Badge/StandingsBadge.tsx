49eeae81b3cb1bc717d2e51a60f998ac53f2575f
João Neto <1539767+joaomlneto@users.noreply.github.com>
2024-01-04 15:55:48 +0100

    Update StandingsBadge.tsx

--- packages/ui/Badge/StandingsBadge.tsx
+++ packages/ui/Badge/StandingsBadge.tsx
@@ -1,26 +1,14 @@
 import { memo } from "react";
-import { Badge, createStyles, Skeleton, type BadgeProps } from "@mantine/core";
+import {
+  Badge,
+  Skeleton,
+  useMantineTheme,
+  type BadgeProps,
+} from "@mantine/core";
 
-const useStyles = createStyles((theme) => ({
-  darkblue: {
-    backgroundColor: "#051468",
-  },
-  lightblue: {
-    backgroundColor: "#224fb7",
-  },
-  gray: {
-    color: theme.black,
-    backgroundColor: "#808080",
-  },
-  orange: {
-    color: theme.black,
-    backgroundColor: "#b53209",
-  },
-  red: {
-    color: theme.black,
-    backgroundColor: "#800007",
-  },
-}));
+
+
+
 
 export type StandingsBadgeProps = BadgeProps & {
   standing?: number;
@@ -28,7 +16,27 @@ export type StandingsBadgeProps = BadgeProps & {
 
 export const StandingsBadge = memo(
   ({ standing, ...otherProps }: StandingsBadgeProps) => {
-    const { classes } = useStyles();
+    const theme = useMantineTheme();
+    const classes = {
+      darkblue: {
+        backgroundColor: "#051468",
+      },
+      lightblue: {
+        backgroundColor: "#224fb7",
+      },
+      gray: {
+        color: theme.black,
+        backgroundColor: "#808080",
+      },
+      orange: {
+        color: theme.black,
+        backgroundColor: "#b53209",
+      },
+      red: {
+        color: theme.black,
+        backgroundColor: "#800007",
+      },
+    };
 
     if (standing === undefined) {
       return (
@@ -44,15 +52,15 @@ export const StandingsBadge = memo(
       standing > 5
         ? classes.darkblue
         : standing > 0
-        ? classes.lightblue
-        : standing == 0
-        ? classes.gray
-        : standing >= -5
-        ? classes.orange
-        : classes.red;
+          ? classes.lightblue
+          : standing == 0
+            ? classes.gray
+            : standing >= -5
+              ? classes.orange
+              : classes.red;
 
     return (
-      <Badge className={className} {...otherProps}>
+      <Badge style={className} {...otherProps}>
         {roundedSecStatus}
       </Badge>
     );
