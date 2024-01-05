4616f8a7c699ebbb38809a554d716920ccdd7f8f
João Neto <1539767+joaomlneto@users.noreply.github.com>
2024-01-05 15:10:59 +0100

    web: add missing css to _app

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -2,6 +2,15 @@
 
 
 import "@mantine/core/styles.css";
+import "@mantine/dates/styles.css";
+import "@mantine/charts/styles.css";
+import "@mantine/notifications/styles.css";
+import "@mantine/code-highlight/styles.css";
+import "@mantine/tiptap/styles.css";
+import "@mantine/dropzone/styles.css";
+import "@mantine/carousel/styles.css";
+import "@mantine/spotlight/styles.css";
+import "@mantine/nprogress/styles.css";
 
 import React, {
   useEffect,
