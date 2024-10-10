1c671cf8eb4f854bce4cd2c4cc00745c9667bbe1
João Neto <1539767+joaomlneto@users.noreply.github.com>
2024-10-10 11:43:31 +0200

    add api endpoint to get market groups tree

---
+++ apps/web/pages/api/market-groups.ts
@@ -0,0 +1,84 @@
+import { type NextApiRequest, type NextApiResponse } from "next";
+
+import { prisma } from "@jitaspace/db";
+
+export type MarketGroupsApiResponseBody = {
+  marketGroups: Record<
+    number,
+    {
+      name: string;
+      parentMarketGroupId: number | null;
+      childrenMarketGroupIds: number[];
+      types: { typeId: number; name: string }[];
+    }
+  >;
+  rootMarketGroupIds: number[];
+};
+
+/**
+ * FIXME: This is a temporary route while the Market Groups tree is not made a server-side component.
+ * This requires moving a lot to the app router, which is on the roadmap.
+ */
+export default async function NextApiRouteHandler(
+  req: NextApiRequest,
+  res: NextApiResponse<MarketGroupsApiResponseBody>,
+) {
+  try {
+    const marketGroups = await prisma.marketGroup.findMany({
+      select: {
+        marketGroupId: true,
+        name: true,
+        parentMarketGroupId: true,
+        children: {
+          select: {
+            marketGroupId: true,
+          },
+        },
+        types: {
+          select: {
+            typeId: true,
+            name: true,
+          },
+        },
+      },
+    });
+
+    const marketGroupsIndex: Record<
+      number,
+      {
+        name: string;
+        parentMarketGroupId: number | null;
+        childrenMarketGroupIds: number[];
+        types: { typeId: number; name: string }[];
+      }
+    > = {};
+    marketGroups.forEach(
+      (marketGroup) =>
+        (marketGroupsIndex[marketGroup.marketGroupId] = {
+          name: marketGroup.name,
+          parentMarketGroupId: marketGroup.parentMarketGroupId,
+          childrenMarketGroupIds: marketGroup.children.map(
+            (marketGroup) => marketGroup.marketGroupId,
+          ),
+          types: marketGroup.types.map((type) => type),
+        }),
+    );
+
+    const rootMarketGroupIds = marketGroups
+      .filter((marketGroup) => marketGroup.parentMarketGroupId === null)
+      .sort((a, b) => a.name.localeCompare(b.name))
+      .map((marketGroup) => marketGroup.marketGroupId);
+
+    return res
+      .setHeader("Cache-Control", "public, max-age=86400") // 24 hours
+      .json({
+        marketGroups: marketGroupsIndex,
+        rootMarketGroupIds,
+      });
+  } catch (e) {
+    return {
+      notFound: true,
+      revalidate: 3600, // 30 seconds on error
+    };
+  }
+}
