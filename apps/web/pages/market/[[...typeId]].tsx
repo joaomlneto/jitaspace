83076019f0c124b6d27ef9be05aeef266a6d6632
João Neto <1539767+joaomlneto@users.noreply.github.com>
2024-10-10 11:44:11 +0200

    attempt to avoid bankrupcy-by-vercel

--- apps/web/pages/market/[[...typeId]].tsx
+++ apps/web/pages/market/[[...typeId]].tsx
@@ -4,26 +4,12 @@ import { GetStaticPaths, GetStaticProps } from "next";
 import { useRouter } from "next/router";
 import { Container, Group, Loader, Stack, Text, Title } from "@mantine/core";
 
-import { prisma } from "@jitaspace/db";
 import { MarketIcon } from "@jitaspace/eve-icons";
 import { useTypeMarketOrders } from "@jitaspace/hooks";
 import { TypeAvatar, TypeName } from "@jitaspace/ui";
 
 import { MarketOrdersDataTable } from "~/components/Market";
 import { MarketLayout } from "~/layouts";
-
-type PageProps = {
-  marketGroups: Record<
-    number,
-    {
-      name: string;
-      parentMarketGroupId: number | null;
-      childrenMarketGroupIds: number[];
-      types: { typeId: number; name: string }[];
-    }
-  >;
-  rootMarketGroupIds: number[];
-};
 
 export const getStaticPaths: GetStaticPaths = async () => {
   return {
@@ -32,60 +18,10 @@ export const getStaticPaths: GetStaticPaths = async () => {
   };
 };
 
-export const getStaticProps: GetStaticProps<PageProps> = async (context) => {
+export const getStaticProps: GetStaticProps<{}> = async (context) => {
   try {
-    const typeIdParam = context.params?.typeId;
-
-    const marketGroups = await prisma.marketGroup.findMany({
-      select: {
-        marketGroupId: true,
-        name: true,
-        parentMarketGroupId: true,
-        children: {
-          select: {
-            marketGroupId: true,
-          },
-        },
-        types: {
-          select: {
-            typeId: true,
-            name: true,
-          },
-        },
-      },
-    });
-
-    const marketGroupsIndex: Record<
-      number,
-      {
-        name: string;
-        parentMarketGroupId: number | null;
-        childrenMarketGroupIds: number[];
-        types: { typeId: number; name: string }[];
-      }
-    > = {};
-    marketGroups.forEach(
-      (marketGroup) =>
-        (marketGroupsIndex[marketGroup.marketGroupId] = {
-          name: marketGroup.name,
-          parentMarketGroupId: marketGroup.parentMarketGroupId,
-          childrenMarketGroupIds: marketGroup.children.map(
-            (marketGroup) => marketGroup.marketGroupId,
-          ),
-          types: marketGroup.types.map((type) => type),
-        }),
-    );
-
-    const rootMarketGroupIds = marketGroups
-      .filter((marketGroup) => marketGroup.parentMarketGroupId === null)
-      .sort((a, b) => a.name.localeCompare(b.name))
-      .map((marketGroup) => marketGroup.marketGroupId);
-
     return {
-      props: {
-        marketGroups: marketGroupsIndex,
-        rootMarketGroupIds,
-      },
+      props: {},
       revalidate: 24 * 3600, // every 24 hours
     };
   } catch (e) {
@@ -96,7 +32,7 @@ export const getStaticProps: GetStaticProps<PageProps> = async (context) => {
   }
 };
 
-export default function Page({ marketGroups, rootMarketGroupIds }: PageProps) {
+export default function Page() {
   const router = useRouter();
   const typeId = router.query.typeId
     ? Number(router.query.typeId as string)
@@ -131,10 +67,7 @@ export default function Page({ marketGroups, rootMarketGroupIds }: PageProps) {
   }
 
   return (
-    <MarketLayout
-      marketGroups={marketGroups}
-      rootMarketGroupIds={rootMarketGroupIds}
-    >
+    <MarketLayout>
       <Container size="xl">
         <Stack gap="xl">
           <Group>
