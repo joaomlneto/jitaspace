f941d6446e0493bfa39d53d911b25851124882c3
João Neto <1539767+joaomlneto@users.noreply.github.com>
2025-02-10 21:00:06 +0100

    next-pwa: remove header metadata

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,3 @@
-
-
-
 import "@mantine/core/styles.css";
 import "@mantine/charts/styles.css";
 import "@mantine/dates/styles.css";
@@ -30,7 +27,6 @@ import { env } from "~/env.mjs";
 import { MyQueryClientProvider } from "~/lib/MyQueryClientProvider";
 import { MySessionProvider } from "~/lib/MySessionProvider";
 
-
 export const metadata = {
   title: "Jita",
   description: "I have followed setup instructions carefully",
@@ -45,59 +41,10 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <ColorSchemeScript defaultColorScheme="dark" />
-        <link
-          rel="apple-touch-icon"
-          sizes="152x152"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#abc2d9" />
-        <meta name="apple-mobile-web-app-title" content="Jita" />
-        <meta name="application-name" content="Jita" />
-        <meta name="msapplication-TileColor" content="#abc2d9" />
-        <meta name="theme-color" content="#abc2d9" />
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
         />
-
-        {/* Progressive Web App (next-pwa) */}
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#9bb4d0" />
-        <meta name="theme-color" content="#9bb4d0" />
-        <link
-          rel="apple-touch-icon"
-          sizes="152x152"
-          href="/apple-touch-icon.png"
-        />
-        <link rel="manifest" href="/manifest.json" />
-        {/*
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:url" content="https://www.jita.space" />
-        <meta name="twitter:title" content="Jita" />
-        <meta name="twitter:description" content="EVE Online Tools" />
-        <meta name="twitter:image" content="/logo.png" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Jita" />
-        <meta property="og:description" content="EVE Online Tools" />
-        <meta property="og:site_name" content="Jita" />
-        <meta property="og:url" content="https://www.jita.space" />
-        <meta property="og:image" content="/api/opengraph/image" />
-        */}
       </head>
       <body>
         <MantineProvider defaultColorScheme="dark" /*theme={themes.default}*/>
