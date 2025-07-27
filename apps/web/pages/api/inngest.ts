7ac0486f29d296efd79563c7562f2a326a57375b
João Neto <1539767+joaomlneto@users.noreply.github.com>
2025-07-27 12:16:14 +0200

    web: increase inngest response body size limit

--- apps/web/pages/api/inngest.ts
+++ apps/web/pages/api/inngest.ts
@@ -2,8 +2,12 @@ import { serve } from "inngest/next";
 
 import { client, functions } from "@jitaspace/eve-scrape";
 
-
-
-
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: "50mb",
+    },
+  },
+};
 
 export default serve({ client, functions });
