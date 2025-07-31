590ef2c0c014d3ef3cdc1ce20bfb45b7b84598f3
João Neto <1539767+joaomlneto@users.noreply.github.com>
2025-07-31 23:46:22 +0200

    web: debug page lists queues and db stats

--- apps/web/pages/debug.tsx
+++ apps/web/pages/debug.tsx
@@ -1,20 +1,77 @@
 import type { ReactElement } from "react";
 import React from "react";
 import { GetStaticProps } from "next";
-import { Container, Group, Stack, Text, Title } from "@mantine/core";
+import {
+  Container,
+  Group,
+  SimpleGrid,
+  Stack,
+  Table,
+  TableData,
+  Text,
+  Title,
+} from "@mantine/core";
 import { NextSeo } from "next-seo";
 
+import { prisma } from "@jitaspace/db";
 import { FolderIcon } from "@jitaspace/eve-icons";
+import { kv } from "@jitaspace/kv";
 
 import { MainLayout } from "~/layouts";
 
 type PageProps = {
+  database: {
+    name: string;
+    count: number;
+  }[];
+  queues: Record<string, string | number>[];
   vars: Record<string, any>;
 };
 
 export const getStaticProps: GetStaticProps<PageProps> = async () => {
+  const queues = Object.values(kv.queues);
+
+  const queuesStatus = await Promise.all(
+    queues.map(async (queue) => ({
+      paused: (await queue.isPaused()) ? "Yes" : "No",
+      size: await queue.count(),
+      active: await queue.getActiveCount(),
+      completed: await queue.getCompletedCount(),
+      failed: await queue.getFailedCount(),
+      delayed: await queue.getDelayedCount(),
+      waiting: await queue.getWaitingCount(),
+      workers: (await queue.getWorkers()).length,
+      name: queue.name,
+    })),
+  );
+
+  const dbModels = Object.keys(prisma).filter(
+    (key) =>
+      !key.startsWith("_") &&
+      !key.startsWith("$") &&
+      ![
+        "constructor",
+        "$connect",
+        "$disconnect",
+        "executeRaw",
+        "queryRaw",
+        "transaction",
+      ].includes(key),
+  );
+  dbModels.sort();
+
+  const dbStats = await Promise.all(
+    dbModels.map(async (name: keyof prisma) => ({
+      name,
+      // @ts-expect-error i don't know how to type this
+      count: await prisma[name as Models].count(),
+    })),
+  );
+
   return {
     props: {
+      database: dbStats,
+      queues: queuesStatus,
       vars: {
         NODE_ENV: process.env.NODE_ENV,
         NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
@@ -35,7 +92,23 @@ export const getStaticProps: GetStaticProps<PageProps> = async () => {
   };
 };
 
-export default function Page({ vars }: Readonly<PageProps>) {
+export default function Page({ database, queues, vars }: Readonly<PageProps>) {
+  const queueKeys = [
+    "name",
+    "paused",
+    "workers",
+    "size",
+    "active",
+    "completed",
+    "failed",
+    "delayed",
+    "waiting",
+  ];
+  const queueTableData: TableData = {
+    head: queueKeys,
+    body: queues.map((queue) => queueKeys.map((key) => queue[key])),
+  };
+
   return (
     <Container size="md">
       <Stack>
@@ -53,6 +126,40 @@ export default function Page({ vars }: Readonly<PageProps>) {
             </div>
           </Group>
         ))}
+        <Group>
+          <FolderIcon width={48} />
+          <Title>Job Queues</Title>
+        </Group>
+        <Table data={queueTableData} />
+        <Group>
+          <FolderIcon width={48} />
+          <Title>Database</Title>
+        </Group>
+        <SimpleGrid
+          cols={{
+            xs: 1,
+            sm: 2,
+            md: 2,
+            lg: 3,
+            xl: 3,
+          }}
+        >
+          {database.map((item) => (
+            <Group
+              key={item.name}
+              justify="space-between"
+              wrap="nowrap"
+              gap="xl"
+            >
+              <div>
+                <Text size="sm">{item.name}</Text>
+              </div>
+              <div>
+                <Text size="sm">{item.count}</Text>
+              </div>
+            </Group>
+          ))}
+        </SimpleGrid>
       </Stack>
     </Container>
   );
