894e94d018b4e02ee9c3031dd093487908453521
João Neto <1539767+joaomlneto@users.noreply.github.com>
2025-07-31 23:46:51 +0200

    kv: add queues for ally/corp/char ids

--- packages/kv/index.ts
+++ packages/kv/index.ts
@@ -14,6 +14,18 @@ const keys = {
 
 export const kv = {
   queues: {
+    allianceIds: new Queue<{ allianceIds: number[] }>(
+      "allianceIds",
+      process.env.REDIS_URL as string,
+    ),
+    characterIds: new Queue<{ characterIds: number[] }>(
+      "characterIds",
+      process.env.REDIS_URL as string,
+    ),
+    corporationIds: new Queue<{ corporationIds: number[] }>(
+      "corporationIds",
+      process.env.REDIS_URL as string,
+    ),
     war: new Queue<GetWarsWarId200[]>("wars", process.env.REDIS_URL as string),
   },
 };
