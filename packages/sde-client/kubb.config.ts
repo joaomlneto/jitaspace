bf2fefea5a511ec3b1baac64a2124239a8759c0f
João Neto <1539767+joaomlneto@users.noreply.github.com>
2025-12-25 20:42:18 +0100

    sde-client: pin version for now

--- packages/sde-client/kubb.config.ts
+++ packages/sde-client/kubb.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(async () => {
     name: "sde-client",
     root: ".",
     input: {
-      path: "http://sde.jita.space/latest/swagger.json",
+      path: "http://sde.jita.space/20250707/swagger.json",
     },
     output: {
       path: "./src/generated",
