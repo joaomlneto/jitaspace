3320a6a286792ae8aee98ab2ef030191b7c6191b
João Neto <1539767+joaomlneto@users.noreply.github.com>
2025-12-26 00:51:07 +0100

    bump next to v16

--- tooling/eslint/base.ts
+++ tooling/eslint/base.ts
@@ -38,7 +38,7 @@ export const restrictEnvAccess = defineConfig(
 
 export const baseConfig = defineConfig(
   // Ignore files not tracked by VCS and any config files
-  includeIgnoreFile(path.join(import.meta.dirname, "../../.gitignore")),
+  includeIgnoreFile(path.join(import.meta?.dirname, "../../.gitignore")),
   { ignores: ["**/*.config.*"] },
   {
     files: ["**/*.js", "**/*.ts", "**/*.tsx"],
