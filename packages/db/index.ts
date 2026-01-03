1d5adf4d6cdfdaca2f8dfb974f85660d0b980a9c
João Neto <1539767+joaomlneto@users.noreply.github.com>
2026-01-03 23:13:08 +0100

    db: fix generation and exports

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,12 +1,15 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-export * from "@prisma/client";
+import { PrismaClient } from "./prisma/generated/client";
 
 const globalForPrisma = globalThis as { prisma?: PrismaClient };
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
+    adapter,
     log:
       process.env.NODE_ENV === "development"
         ? ["query", "info", "error", "warn"]
@@ -14,3 +17,5 @@ export const prisma =
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export * from "./prisma/generated/client";
