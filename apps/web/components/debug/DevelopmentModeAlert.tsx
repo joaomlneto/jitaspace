921cca6f5c324ad1abcf7ecff1c2798e02a4d131
João Neto <1539767+joaomlneto@users.noreply.github.com>
2026-01-04 01:03:07 +0100

    web: fix prisma console port

--- apps/web/components/debug/DevelopmentModeAlert.tsx
+++ apps/web/components/debug/DevelopmentModeAlert.tsx
@@ -16,7 +16,7 @@ export const DevelopmentModeAlert = (props: DevelopmentModeAlertProps) => {
         <Anchor component={Link} href="http://localhost:8288" target="_blank">
           <Button>Inngest UI</Button>
         </Anchor>
-        <Anchor component={Link} href="http://localhost:5555" target="_blank">
+        <Anchor component={Link} href="http://localhost:51212" target="_blank">
           <Button>Prisma UI</Button>
         </Anchor>
       </Group>
