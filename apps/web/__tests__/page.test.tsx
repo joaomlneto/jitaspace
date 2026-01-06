fd51a1a46956ae1e00178149b9b3c7ab02f3dadf
João Neto <1539767+joaomlneto@users.noreply.github.com>
2026-01-06 20:33:01 +0100

    web: fix jest

---
+++ apps/web/__tests__/page.test.tsx
@@ -0,0 +1,21 @@
+
+
+
+import "@testing-library/jest-dom";
+
+
+
+
+
+//import Page from "../pages/index";
+
+
+describe("Page", () => {
+  it("does nothing", () => {
+    //render(<Page />);
+
+    //const heading = screen.getByRole("heading", { level: 1 });
+
+    //expect(heading).toBeInTheDocument();
+  });
+});
