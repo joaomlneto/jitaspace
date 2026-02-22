e75b2e4cb312089e5e53daef66ba5f8b2bd6b62a
João Neto <1539767+joaomlneto@users.noreply.github.com>
2026-02-22 23:29:59 +0100

    Update CCP SDE metadata URL
    Replace HEAD request to the S3 SDE zip with a JSON fetch to developers.eveonline.com/latest.jsonl

--- apps/web/pages/api/sde-last-modified.ts
+++ apps/web/pages/api/sde-last-modified.ts
@@ -1,26 +1,24 @@
-import type {NextApiRequest, NextApiResponse} from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function NextApiRouteHandler(
   req: NextApiRequest,
   res: NextApiResponse<
     | { error: string }
     | {
-        date: string | null;
-        lastModified: string | null;
+        _key: "sde";
+        buildNumber: number;
+        releaseDate: string;
       }
   >,
 ) {
-  const sdeResponse = await fetch(
-    `https://eve-static-data-export.s3-eu-west-1.amazonaws.com/tranquility/sde.zip`,
-    {
-      method: "HEAD",
-    },
-  );
+  const response: {
+    _key: "sde";
+    buildNumber: number;
+    releaseDate: string;
+  } = await fetch(
+    `https://developers.eveonline.com/static-data/tranquility/latest.jsonl`,
+  ).then((res) => res.json());
 
-  const response = {
-    date: sdeResponse.headers.get("date"),
-    lastModified: sdeResponse.headers.get("last-modified"),
-  };
 
   return res
     .setHeader("Cache-Control", "public, max-age=300") // 5 minutes
