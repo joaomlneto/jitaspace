95c3d88fdbc2bf2effd2e04c1c9048c381b2b404
João Neto <1539767+joaomlneto@users.noreply.github.com>
2026-02-24 22:21:27 +0100

    remove unused pages

--- apps/web/app/testapp/layout.tsx
+++
@@ -1,8 +0,0 @@
-import type { PropsWithChildren } from "react";
-
-import { MainLayout } from "~/layouts";
-
-
-export default function Layout({ children }: PropsWithChildren) {
-  return <MainLayout>{children}</MainLayout>;
-}
