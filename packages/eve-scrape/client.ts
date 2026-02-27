15f9385fa4f71a8a4f764fedb8b101eb1d81301b
João Neto <1539767+joaomlneto@users.noreply.github.com>
2026-02-27 23:19:18 +0100

    eve-scrape: fix use of wrong inngest key

--- packages/eve-scrape/client.ts
+++ packages/eve-scrape/client.ts
@@ -6,6 +6,6 @@ import { Events } from "./events";
 // Create a client to send and receive events
 export const client = new Inngest({
   id: slugify("jitaspace"),
-  eventKey: env.INNGEST_SIGNING_KEY,
+  eventKey: env.INNGEST_EVENT_KEY,
   schemas: new EventSchemas().fromRecord<Events>(),
 });
